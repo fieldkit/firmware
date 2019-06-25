65d4c7b7696eef3a7fb0be44458c1ef52ed1026c
Jacob Lewallen <jlewallen@gmail.com>
2019-06-25 16:25:36 -0700

    Saving state with WiFi and GPS checks working.

--- fk/main.cpp
+++ fk/main.cpp
@@ -5,6 +5,9 @@
 #include "self_check.h"
 
 #include <Arduino.h>
+#include <WiFi101.h>
+#include <SPI.h>
+#include <Wire.h>
 
 using namespace fk;
 
@@ -26,8 +29,137 @@ void run_tasks() {
     OS_CHECK(os_start());
 }
 
+/**
+ * RADIO_MOSI  PB26
+ * RADIO_MISO  PB29
+ * RADIO_SCK   PB27
+
+ * WIFI_CS     PB05
+ * WIFI_ENABLE PC25
+ * WIFI_RESET  PC26
+ * WIFI_IRQ    PC27
+
+ * WIFI_POWER  PB07
+ *
+ * GPS_TX      PB24
+ * GPS_RX      PB25
+ * GPS_POWER   PB06
+ */
+
+constexpr uint8_t WINC1500_CS = 54u;
+constexpr uint8_t WINC1500_ENABLE = 95u;
+constexpr uint8_t WINC1500_IRQ = 97u;
+constexpr uint8_t WINC1500_RESET = 96u;
+constexpr uint8_t WINC1500_POWER = 56u;
+constexpr uint8_t GPS_POWER = 55u;
+
+void gps() {
+    fkb_external_println("fk: checking gps");
+
+    auto working = false;
+
+    uint32_t started = fk_uptime();
+    while ((fk_uptime() - started) < 5000) {
+        if (Serial1.available()) {
+            auto c = Serial1.read();
+            SEGGER_RTT_PutChar(0, c);
+            working = true;
+        }
+    }
+
+    if (working) {
+        fkb_external_println("fk: gps found!");
+    }
+    else {
+        fkb_external_println("fk: no gps!");
+    }
+}
+
+void wifi() {
+    fkb_external_println("fk: checking wifi");
+
+    SPI1.begin();
+
+    WiFi.setPins(WINC1500_CS, WINC1500_IRQ, WINC1500_RESET);
+
+    if (WiFi.status() == WL_NO_SHIELD) {
+        fkb_external_println("fk: no wifi");
+        return;
+    }
+
+    WiFi.end();
+
+    fkb_external_println("fk: wifi found!");
+}
+
+
+uint16_t read_u16(uint8_t address, uint8_t reg) {
+    Wire1.beginTransmission(address);
+    Wire1.write(reg);
+    Wire1.endTransmission();
+    Wire1.requestFrom(address, 2);
+    uint16_t value  = Wire1.read();
+    value |= (uint16_t)Wire1.read() << 8;
+    return value;
+}
+
+bool write_u16(uint8_t address, uint8_t reg, uint16_t value) {
+    Wire1.beginTransmission(address);
+    Wire1.write(reg);
+    Wire1.write( value       & 0xFF);
+    Wire1.write((value >> 8) & 0xFF);
+    return Wire1.endTransmission() == 0;
+}
+
+void fuel_gauge() {
+    Wire1.begin();
+
+    for (auto i = 0; i < 0x128; ++i) {
+        auto address = 0x36;
+
+        if (!write_u16(address, 0x18, 2000 * 2)) {
+            fkb_external_println("fk: failed to write capacity");
+            break;
+        }
+
+        auto raw_voltage = read_u16(address, 0x09);
+        auto voltage = (float)raw_voltage * 7.8125e-5f;
+
+        fkb_external_println("fk: raw voltage=%d", raw_voltage);
+        fkb_external_println("fk: %f", voltage);
+
+        break;
+    }
+}
+
 void setup() {
     fkb_external_println("fk: hello!");
+
+    pinMode(WINC1500_CS, OUTPUT);
+    pinMode(WINC1500_IRQ, INPUT);
+    pinMode(WINC1500_RESET, OUTPUT);
+
+    pinMode(GPS_POWER, OUTPUT);
+    digitalWrite(GPS_POWER, LOW);
+    delay(100);
+    digitalWrite(GPS_POWER, HIGH);
+    delay(100);
+
+    pinMode(WINC1500_POWER, OUTPUT);
+    digitalWrite(WINC1500_POWER, LOW);
+    delay(100);
+    digitalWrite(WINC1500_POWER, HIGH);
+    delay(100);
+
+    Serial1.begin(9600);
+
+    while (true) {
+        fuel_gauge();
+
+        wifi();
+
+        delay(500);
+    }
 
     DisplayFactory display_factory;
     Display *display = display_factory.get_display();
