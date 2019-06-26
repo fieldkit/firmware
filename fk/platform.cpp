6e2ca8b861e3e578673d7a23eb1a9a6d179c2336
Jacob Lewallen <jlewallen@gmail.com>
2019-06-26 14:38:15 -0700

    Expose fkb_external_print stuff for now.

--- fk/platform.cpp
+++ fk/platform.cpp
@@ -1,11 +1,50 @@
+#include <cstdarg>
+
 #include "fk.h"
 #include "platform.h"
+
+#if defined(ARDUINO)
 #include <Arduino.h>
+#endif
 
 namespace fk {
+
+#if defined(ARDUINO)
 
 uint32_t fk_uptime() {
     return millis();
 }
 
+#else
+#endif
+
 }
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(__SAMD51__)
+
+#else
+
+uint32_t fkb_external_printf(const char *str, ...) {
+    va_list args;
+    va_start(args, str);
+    vfprintf(stderr, str, args);
+    va_end(args);
+}
+
+uint32_t fkb_external_println(const char *str, ...) {
+    va_list args;
+    va_start(args, str);
+    vfprintf(stderr, str, args);
+    fprintf(stderr, "\n");
+    va_end(args);
+}
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
--- fk/platform.h
+++ fk/platform.h
@@ -10,3 +10,15 @@ namespace fk {
 uint32_t fk_uptime();
 
 }
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint32_t fkb_external_printf(const char *str, ...);
+
+uint32_t fkb_external_println(const char *str, ...);
+
+#ifdef __cplusplus
+}
+#endif
