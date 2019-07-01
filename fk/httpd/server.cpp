63e3c5b7efdf64328167ae4a89cc1f65a8735a03
Jacob Lewallen <jlewallen@gmail.com>
2019-07-01 07:18:27 -0700

    Add a connection pool.

--- fk/httpd/server.cpp
+++ fk/httpd/server.cpp
@@ -1,4 +1,5 @@
 #include <cstring>
+#include <os.h>
 
 #include "common.h"
 #include "platform.h"
@@ -39,59 +40,119 @@ bool HttpServer::begin() {
     return true;
 }
 
-void HttpServer::tick() {
-    auto connection = wifi_->accept();
-    if (connection == nullptr) {
-        return;
+class ConnectionPool {
+private:
+    typedef struct Connection {
+        WifiConnection *c{ nullptr };
+        HttpRequest req;
+        uint8_t data[1024];
+        size_t position;
+    } Connection;
+
+    constexpr static size_t MaximumConnections = 4;
+    Connection pool_[MaximumConnections];
+
+public:
+    size_t available() {
+        size_t used = 0;
+        for (auto i = (size_t)0; i < MaximumConnections; ++i) {
+            if (pool_[i].c != nullptr) {
+                used++;
+            }
+        }
+        return MaximumConnections - used;
     }
 
-    fkinfo("connection!");
-
-    uint8_t buffer[1024] = { 0 };
-    size_t position = 0;
-    HttpRequest req;
-
-    // TODO: 414 Request-URI Too Long
-    // TODO: 431 Request Header Fields Too Large.
-    // TODO: 413 Payload Too Large
-    // TODO: 500 Service Unavailable
-    // TODO: 503 Service Unavailable
-
-    while (connection->status() == WifiConnectionStatus::Connected) {
-        if (connection->available()) {
-            auto available = sizeof(buffer) - position;
-            auto nread = connection->read(buffer, available);
-            if (nread < 0) {
-                fkinfo("EOS read");
-                continue;
+    void service() {
+        for (auto i = (size_t)0; i < MaximumConnections; ++i) {
+            if (pool_[i].c != nullptr) {
+                if (!service(&pool_[i])) {
+                    // fkinfo("%d removing", i);
+                    pool_[i].c->stop();
+                    delete pool_[i].c;
+                    pool_[i] = { };
+                }
             }
-            if (nread == 0) {
-                fkinfo("empty read");
-                continue;
-            }
-
-            if (req.parse((const char *)(buffer + position), nread) != 0) {
-            }
-
-            if (req.have_headers()) {
-            }
-
-            if (req.consumed()) {
-                connection->write("HTTP/1.1 204 OK\n");
-                connection->write("Content-Length: 0\n");
-                connection->write("Content-Type: text/html\n");
-                connection->write("Connection: close\n");
-                connection->write("\n");
-                break;
-            }
-
-            position += nread;
         }
     }
 
-    connection->stop();
+    void queue(WifiConnection *c) {
+        for (auto i = (size_t)0; i < MaximumConnections; ++i) {
+            if (pool_[i].c == nullptr) {
+                // fkinfo("%d queued", i);
+                pool_[i].c = c;
+                pool_[i].position = 0;
+                pool_[i].req.begin();
+                return;
+            }
+        }
+        FK_ASSERT(false);
+    }
 
-    delete connection;
+private:
+    bool service(Connection *c) {
+        // TODO: 414 Request-URI Too Long
+        // TODO: 431 Request Header Fields Too Large.
+        // TODO: 413 Payload Too Large
+        // TODO: 500 Service Unavailable
+        // TODO: 503 Service Unavailable
+        if (c->c->status() != WifiConnectionStatus::Connected) {
+            return false;
+        }
+
+        if (c->c->available()) {
+            auto available = sizeof(c->data) - c->position;
+            auto nread = c->c->read(c->data, available);
+
+            c->data[c->position + nread] = 0;
+
+            if (nread < 0) {
+                fkinfo("EOS read");
+                return false;
+            }
+
+            if (nread == 0) {
+                fkinfo("empty read");
+                return false;
+            }
+
+            if (c->req.parse((const char *)(c->data + c->position), nread) != 0) {
+                fkinfo("parse error");
+                return false;
+            }
+
+            c->position += nread;
+        }
+
+        if (c->req.have_headers()) {
+        }
+
+        if (c->req.consumed()) {
+            c->c->write("HTTP/1.1 204 OK\n");
+            c->c->write("Content-Length: 0\n");
+            c->c->write("Content-Type: text/html\n");
+            c->c->write("Connection: close\n");
+            c->c->write("\n");
+            return false;
+        }
+
+        return true;
+    }
+
+};
+
+ConnectionPool pool;
+
+void HttpServer::tick() {
+    if (pool.available() > 0) {
+        auto connection = wifi_->accept();
+        if (connection != nullptr) {
+            fkinfo("connection (0x%p) %ld", connection, connection->socket());
+            pool.queue(connection);
+        }
+    }
+
+    pool.service();
 }
 
 void HttpServer::stop() {
