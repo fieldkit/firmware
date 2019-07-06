f8f860877a1f1ab624326f02977ccf037868b305
Jacob Lewallen <jlewallen@gmail.com>
2019-07-06 09:30:56 -0700

    Adding ASF4 library.

--- cmake/FindArduinoMDNS.cmake
+++ cmake/FindArduinoMDNS.cmake
@@ -11,7 +11,7 @@ file(GLOB sources ${ArduinoMDNS_PATH}/*.cpp ${ArduinoMDNS_PATH}/utility/*.c)
 
 add_arduino_library(ArduinoMDNS "${sources}")
 
-target_include_directories(ArduinoMDNS 
+target_include_directories(ArduinoMDNS
   PUBLIC ${ArduinoMDNS_PATH}
   PRIVATE ${ArduinoMDNS_PATH}
 )
---
+++ cmake/FindAsf4.cmake
@@ -0,0 +1,19 @@
+#
+#
+#
+if(TARGET Asf4)
+  return()
+endif()
+
+message("${asf4_PATH}")
+
+file(GLOB sources ${asf4_PATH}/samd51/hal/src/*.c ${asf4_PATH}/samd51/hpl/*/*.c)
+
+add_arduino_library(Asf4 "${sources}")
+
+target_include_directories(Asf4
+  PUBLIC ${asf4_PATH}/samd51/config ${asf4_PATH}/samd51/include ${asf4_PATH}/samd51/hri
+  ${asf4_PATH}/samd51/hal/include
+  ${asf4_PATH}/samd51/hal/utils/include ${asf4_PATH}/samd51/hpl/core ${asf4_PATH}/samd51/hpl/gclk
+  ${asf4_PATH}/samd51/hpl/port ${asf4_PATH}/samd51/hpl/tc
+)
