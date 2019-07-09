c07bddeb21718219d300f2dd06ad0baea2c25885
Jacob Lewallen <jlewallen@gmail.com>
2019-07-09 11:45:42 -0700

    Link tests and fake to phylum.

--- cmake/FindPhylum.cmake
+++ cmake/FindPhylum.cmake
@@ -4,15 +4,23 @@ endif()
 
 set(phylum_RECURSE True)
 
-add_external_arduino_library(phylum)
+if(${TARGET_ARCH} MATCHES "amd64")
+  file(GLOB_RECURSE sources ${phylum_PATH}/src/*.cpp ${phylum_PATH}/src/*.c)
+  add_library(phylum STATIC ${sources})
 
-find_package(arduino-logging)
-target_link_libraries(phylum arduino-logging)
+  find_package(arduino-logging)
+  target_link_libraries(phylum arduino-logging)
+else()
+  add_external_arduino_library(phylum)
 
-find_package(SPI)
-target_link_libraries(phylum SPI)
+  find_package(arduino-logging)
+  target_link_libraries(phylum arduino-logging)
 
-target_compile_options(phylum PUBLIC -DPHYLUM_ENABLE_SD)
+  find_package(SPI)
+  target_link_libraries(phylum SPI)
+
+  target_compile_options(phylum PUBLIC -DPHYLUM_ENABLE_SD)
+endif()
 
 target_include_directories(phylum
     PUBLIC ${phylum_PATH}/src
