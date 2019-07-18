1cc04a8bb61b7d42b2d21dee39274d3406d3e9b8
Jacob Lewallen <jlewallen@gmail.com>
2019-07-18 16:54:22 -0700

    Malformed I2C handling.

--- fk/temperature.cpp
+++ fk/temperature.cpp
@@ -15,9 +15,6 @@ bool CoreTemperature::read(float *temperature) {
     wire_->requestFrom(0x48, 2);
     auto lsb = wire_->read();
     auto msb = wire_->read();
-    if (!I2C_CHECK(wire_->endTransmission())) {
-        return false;
-    }
 
     auto temp_data = ((lsb << 8) | msb) >> 5;
 
