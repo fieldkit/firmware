0166e414aa6cd622041debe31e80417d5f574b3e
Jacob Lewallen <jlewallen@gmail.com>
2019-07-18 17:31:30 -0700

    Write some fake data to storage in readings task.

--- fk/main.cpp
+++ fk/main.cpp
@@ -9,6 +9,7 @@
 #include "self_check.h"
 #include "storage.h"
 #include "clock.h"
+#include "protobuf.h"
 
 #include "tiny_printf.h"
 #include "secrets.h"
@@ -132,6 +133,55 @@ static void task_handler_gps(void *params) {
 }
 
 static void task_handler_readings(void *params) {
+    auto memory = MemoryFactory::get_data_memory();
+    Storage storage{ memory };
+
+    if (!storage.begin()) {
+        return;
+    }
+
+    auto now = get_clock()->now().unixtime();
+
+    auto file = storage.file(0);
+
+    fk_data_SensorAndValue readings[] = {
+        { 0, (float)random(20, 100) },
+        { 1, (float)random(20, 100) },
+        { 2, (float)random(20, 100) },
+        { 3, (float)random(20, 100) },
+        { 4, (float)random(20, 100) },
+        { 5, (float)random(20, 100) },
+        { 6, (float)random(20, 100) },
+        { 7, (float)random(20, 100) },
+        { 8, (float)random(20, 100) },
+        { 9, (float)random(20, 100) },
+    };
+
+    pb_array_t readings_array = {
+        .length = (size_t)10,
+        .itemSize = sizeof(fk_data_SensorAndValue),
+        .buffer = &readings,
+        .fields = fk_data_SensorAndValue_fields,
+    };
+
+    fk_data_DataRecord record = fk_data_DataRecord_init_default;
+    record.readings.time = now;
+    record.readings.reading = file.record();
+    record.readings.flags = 0;
+    record.readings.location.fix = 0;
+    record.readings.location.time = now;
+    record.readings.location.longitude = -118.2709223;
+    record.readings.location.latitude = 34.0318047;
+    record.readings.location.altitude = 100.0f;
+    record.readings.readings.funcs.encode = pb_encode_array;
+    record.readings.readings.arg = &readings_array;
+
+    auto bytes_wrote = file.write(&record, fk_data_DataRecord_fields);
+    if (bytes_wrote == 0) {
+        logerror("error saving readings");
+    }
+
+    loginfo("wrote %d bytes (%d bytes)", bytes_wrote, file.size());
 }
 
 void run_tasks() {
@@ -243,6 +293,10 @@ void setup() {
 
         fk_delay(1000);
     }
+
+    // Call this here because things go horribly if we call from within a task.
+    // Something goes south with a malloc.
+    random(100, 1000);
 
     run_tasks();
 }
