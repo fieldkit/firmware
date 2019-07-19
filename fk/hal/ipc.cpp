aa98d9609536e1050303d2244f19a1d5c079a182
Jacob Lewallen <jlewallen@gmail.com>
2019-07-19 13:35:36 -0700

    Rename "queue" :)

--- fk/hal/ipc.cpp
+++ fk/hal/ipc.cpp
@@ -12,23 +12,23 @@ IPC *get_ipc() {
 
 #if defined(FK_HARDWARE_FULL)
 
-os_queue_define(queue, 10);
-os_queue_define(data_queue, 10);
+os_queue_define(other_queue, 10, OS_QUEUE_FLAGS_NONE);
+os_queue_define(data_queue, 10, OS_QUEUE_FLAGS_QUEUE_ONLY);
 
 bool IPC::begin() {
-    OS_CHECK(os_queue_create(os_queue(queue), os_queue_def(queue)));
+    OS_CHECK(os_queue_create(os_queue(other_queue), os_queue_def(other_queue)));
     OS_CHECK(os_queue_create(os_queue(data_queue), os_queue_def(data_queue)));
 
     return true;
 }
 
 bool IPC::enqueue(void *ptr, uint32_t to) {
-    auto tuple = os_queue_enqueue(os_queue(queue), ptr, to);
+    auto tuple = os_queue_enqueue(os_queue(other_queue), ptr, to);
     return tuple.status == OSS_SUCCESS;
 }
 
 bool IPC::dequeue(void **ptr, uint32_t to) {
-    auto tuple = os_queue_dequeue(os_queue(queue), to);
+    auto tuple = os_queue_dequeue(os_queue(other_queue), to);
     if (tuple.status != OSS_SUCCESS) {
         return false;
     }
