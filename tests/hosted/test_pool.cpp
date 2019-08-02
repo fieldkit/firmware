67a4125794f267e7c1fa671634997f2c9ab3ad27
Jacob Lewallen <jlewallen@gmail.com>
2019-08-01 19:55:00 -0700

    Pool tests.

---
+++ tests/hosted/test_pool.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+
+#include "pool.h"
+
+using namespace fk;
+
+class PoolSuite : public ::testing::Test {
+protected:
+
+};
+
+TEST_F(PoolSuite, Basic) {
+    StaticPool<1024> pool("Pool");
+
+    void *p1 = pool.malloc(256);
+    ASSERT_NE(p1, nullptr);
+    ASSERT_EQ(pool.allocated(), (size_t)256);
+
+    void *p2 = pool.malloc(256);
+    ASSERT_NE(p2, nullptr);
+    ASSERT_EQ(pool.allocated(), (size_t)512);
+
+    pool.clear();
+    ASSERT_EQ(pool.allocated(), (size_t)0);
+}
+
+TEST_F(PoolSuite, Alignment) {
+    StaticPool<1024> pool("Pool");
+
+    void *p1 = pool.malloc(13);
+    ASSERT_NE(p1, nullptr);
+    ASSERT_EQ(pool.allocated(), (size_t)16);
+}
+
+TEST_F(PoolSuite, Subpool) {
+    StaticPool<2048> pool("Pool");
+
+    void *p1 = pool.malloc(256);
+    ASSERT_NE(p1, nullptr);
+    ASSERT_EQ(pool.allocated(), (size_t)256);
+    ASSERT_FALSE(pool.frozen());
+
+    Pool child = pool.freeze("Child");
+    ASSERT_EQ(child.size(), (size_t)(2048 - 256));
+    ASSERT_EQ(child.allocated(), (size_t)0);
+
+    ASSERT_TRUE(pool.frozen());
+
+    pool.clear();
+    ASSERT_FALSE(pool.frozen());
+}
