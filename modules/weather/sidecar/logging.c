86b65387e8af68d328b60e756a46b2c796d9cecf
Jacob Lewallen <jlewallen@gmail.com>
2019-08-05 16:49:51 -0700

    Separate loginfo that does format strings.

--- modules/weather/sidecar/logging.c
+++ modules/weather/sidecar/logging.c
@@ -1,19 +1,22 @@
 #include "logging.h"
 
-int32_t loginfo(const char *f, ...) {
+int32_t loginfo(const char *f) {
+    SEGGER_RTT_WriteString(0, RTT_CTRL_TEXT_GREEN "weather " RTT_CTRL_TEXT_YELLOW "info " RTT_CTRL_RESET);
+    SEGGER_RTT_WriteString(0, f);
+    SEGGER_RTT_WriteString(0, RTT_CTRL_RESET "\n");
+}
+
+int32_t loginfof(const char *f, ...) {
     va_list args;
     va_start(args, f);
     SEGGER_RTT_WriteString(0, RTT_CTRL_TEXT_GREEN "weather " RTT_CTRL_TEXT_YELLOW "info " RTT_CTRL_RESET);
-    SEGGER_RTT_WriteString(0, f);
+    SEGGER_RTT_vprintf(0, f, &args);
     SEGGER_RTT_WriteString(0, RTT_CTRL_RESET "\n");
     va_end(args);
 }
 
-int32_t logerror(const char *f, ...) {
-    va_list args;
-    va_start(args, f);
+int32_t logerror(const char *f) {
     SEGGER_RTT_WriteString(0, RTT_CTRL_TEXT_GREEN "weather " RTT_CTRL_TEXT_RED "error ");
     SEGGER_RTT_WriteString(0, f);
     SEGGER_RTT_WriteString(0, RTT_CTRL_RESET "\n");
-    va_end(args);
 }
--- modules/weather/sidecar/logging.h
+++ modules/weather/sidecar/logging.h
@@ -4,5 +4,6 @@
 
 #include <SEGGER_RTT.h>
 
-int32_t loginfo(const char *f, ...);
-int32_t logerror(const char *f, ...);
+int32_t loginfo(const char *f);
+int32_t loginfof(const char *f, ...);
+int32_t logerror(const char *f);
