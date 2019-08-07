a2359676c84b64cbf10ab27a7d8b28d1045688e1
Jacob Lewallen <jlewallen@gmail.com>
2019-08-07 16:12:55 -0700

    Clear ADC value.

--- modules/weather/sidecar/adc081c.c
+++ modules/weather/sidecar/adc081c.c
@@ -21,6 +21,8 @@ int32_t adc081c_initialize(struct i2c_m_sync_desc *i2c) {
 int32_t adc081c_reading_get(struct i2c_m_sync_desc *i2c, adc081c_reading_t *reading) {
     int32_t rv;
 
+    reading->value = 0;
+
     rv = i2c_read_u16(i2c, ADC081C_I2C_ADDRESS, ADC081C_REGISTER_READING, &reading->value);
     if (rv != FK_SUCCESS) {
         return rv;
