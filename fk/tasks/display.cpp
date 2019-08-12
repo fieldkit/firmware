f27cdd21f7e4fbaa6e6ed8e3b1903f232c5086b3
Jacob Lewallen <jlewallen@gmail.com>
2019-08-12 16:52:18 -0700

    Encode serial number in QR.

--- fk/tasks/display.cpp
+++ fk/tasks/display.cpp
@@ -34,10 +34,17 @@ static void show_qr() {
     auto bus = get_board()->i2c_core();
     auto display = get_display();
 
+    fk_serial_number_t sn;
+    fk_serial_number_get(&sn);
+
+    // Version 3 (29x29) LOW ECC
+    // Numeric = 127
+    // Alphanumeric = 77
+    // Bytes = 53
     QRCode qr;
     uint8_t version = 3;
     uint8_t data[qrcode_getBufferSize(version)];
-    qrcode_initText(&qr, data, version, ECC_LOW, "HELLO WORLD");
+    qrcode_initBytes(&qr, data, version, ECC_LOW, (uint8_t *)&sn, sizeof(sn));
 
     QrCodeScreen screen;
     screen.size = qr.size;
