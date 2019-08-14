4269598ad77fbcdc96edc68bc0e9b9c87a7d146d
Jacob Lewallen <jlewallen@gmail.com>
2019-08-14 15:12:46 -0700

    Create peripheral I2C mutex.

--- fk/main.cpp
+++ fk/main.cpp
@@ -91,6 +91,7 @@ static void run_tasks() {
     FK_ASSERT(get_ipc()->begin());
 
     FK_ASSERT(storage_mutex.create());
+    FK_ASSERT(peripheral_i2c_core_mutex.create());
 
     OS_CHECK(os_start());
 }
