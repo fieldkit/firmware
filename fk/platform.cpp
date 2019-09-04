fd34e66503b88c17ebd1d265fff4bccd4881519f
Jacob Lewallen <jlewallen@gmail.com>
2019-09-04 16:02:01 -0700

    I prefer lowercase hex.

--- fk/platform.cpp
+++ fk/platform.cpp
@@ -136,12 +136,12 @@ extern "C" {
 
 void *fk_malloc_internal(size_t size, const char *file, int32_t line) {
     auto ptr = ::malloc(size);
-    alogf(LogLevels::DEBUG, "mem", "[0x%p] malloc(%zd) %s:%" PRId32 "", ptr, size, file, line);
+    alogf(LogLevels::DEBUG, "mem", "[0x%" PRIxPTR "] malloc(%zd) %s:%" PRId32 "", (uintptr_t)ptr, size, file, line);
     return ptr;
 }
 
 void fk_free_internal(void *ptr, const char *file, int32_t line) {
-    alogf(LogLevels::DEBUG, "mem", "[0x%p] free %s:%" PRId32 "", ptr, file, line);
+    alogf(LogLevels::DEBUG, "mem", "[0x%" PRIxPTR "] free %s:%" PRId32 "", (uintptr_t)ptr, file, line);
     return ::free(ptr);
 }
 
