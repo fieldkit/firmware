18be71c94e7d872c75cb6ae0788f3af853f82a68
Jacob Lewallen <jlewallen@gmail.com>
2019-10-09 16:27:17 -0700

    Finish up the Ultrasonic firmware.

--- modules/ultrasonic/main/ultrasonic_module.cpp
+++ modules/ultrasonic/main/ultrasonic_module.cpp
@@ -9,31 +9,31 @@ UltrasonicModule::UltrasonicModule() : bridge_(get_board()->acquire_i2c_module()
 }
 
 bool UltrasonicModule::initialize(ModuleContext mc, fk::Pool &pool) {
-    loginfo("hello");
-
     auto bus = get_board()->i2c_module();
 
+    /*
+    // enumerate bus
     for (auto i = 0u; i < 128u; ++i) {
         auto rv = bus.write(i, nullptr, 0);
         if (I2C_CHECK(rv)) {
             loginfo("found 0x%x", i);
         }
     }
+    */
 
     if (!bridge_.begin(9600)) {
         logerror("initializing bridge");
-        FK_ASSERT(0);
         return false;
     }
-
-    FK_ASSERT(0);
 
     return true;
 }
 
 static SensorMetadata const fk_module_ultrasonic_sensor_metas[] = {
-    { .name = "calibration", .unitOfMeasure = "cm", .flags = 0 },
-    { .name = "distance",    .unitOfMeasure = "cm", .flags = 0 },
+    { .name = "distance_0",  .unitOfMeasure = "mm", .flags = 0 },
+    { .name = "distance_1",  .unitOfMeasure = "mm", .flags = 0 },
+    { .name = "distance_2",  .unitOfMeasure = "mm", .flags = 0 },
+    { .name = "calibration", .unitOfMeasure = "mm", .flags = 0 },
 };
 
 static ModuleSensors fk_module_ultrasonic_sensors = {
@@ -46,15 +46,37 @@ ModuleSensors const *UltrasonicModule::get_sensors(Pool &pool) {
 }
 
 ModuleReadings *UltrasonicModule::take_readings(ModuleContext mc, fk::Pool &pool) {
-    const char *line = nullptr;
-    if (!line_reader_.read_line_sync(&line, 1000)) {
-        logerror("error reading line");
+    auto mr = new(pool) NModuleReadings<4>();
+    auto nreadings = 0u;
+
+    for (auto i = 0; i < 5; ++i) {
+        const char *line = nullptr;
+        if (!line_reader_.read_line_sync(&line, 1000)) {
+            logerror("error reading line");
+        }
+
+        if (line != nullptr) {
+            if (line[0] == 'R') {
+                auto value = atof(line + 1);
+                mr->set(nreadings++, value);
+                loginfo("[%d] line: %s", nreadings, line);
+                if (nreadings == 3) {
+                    break;
+                }
+            }
+            else {
+                logwarn("[%d] line: %s", nreadings, line);
+            }
+        }
+    }
+
+    if (nreadings < 3) {
+        logerror("unable to gather enough readings (%d)", nreadings);
         return nullptr;
     }
 
-    if (line != nullptr) {
-        loginfo("line: %s", line);
-    }
+    // Calibration value
+    mr->set(nreadings++, (float)0.0f);
 
-    return nullptr;
+    return mr;
 }
