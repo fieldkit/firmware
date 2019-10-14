f7f9187f284e4d1718e28a610016b74267463d39
Jacob Lewallen <jlewallen@gmail.com>
2019-10-14 13:18:10 -0700

    Fix estimating lora packet size.

--- fk/lora_packetizer.cpp
+++ fk/lora_packetizer.cpp
@@ -15,6 +15,7 @@ LoraPacketizer::~LoraPacketizer() {
 class LoraRecord {
 private:
     static constexpr size_t MaxReadingsPerPacket = 32;
+    static constexpr size_t TagSize = 1;
 
 private:
     Pool *pool_;
@@ -26,17 +27,7 @@ private:
 
 public:
     LoraRecord(Pool &pool) : pool_(&pool) {
-        record_ = fk_data_LoraRecord_init_default;
-        record_.values.funcs.encode = pb_encode_array;
-        record_.time = 0;
-        record_.number = 0;
-        record_.module = 0;
-
-        values_array_ = {
-            .length = 0,
-            .itemSize = sizeof(float),
-            .buffer = (void *)&values_,
-        };
+        clear();
     }
 
 public:
@@ -49,22 +40,41 @@ public:
         record_.time = time;
         record_.number = reading;
 
-        encoded_size_ += pb_varint_size(record_.time) + 1;
-        encoded_size_ += pb_varint_size(record_.number) + 1;
+        FK_ASSERT(time > 0);
+
+        encoded_size_ = 0;
+        encoded_size_ += record_.time > 0 ? pb_varint_size(record_.time) + TagSize : 0;
+        encoded_size_ += record_.number > 0 ? pb_varint_size(record_.number) + TagSize : 0;
+    }
+
+    void clear() {
+        record_ = fk_data_LoraRecord_init_default;
+        record_.values.funcs.encode = pb_encode_array;
+        record_.time = 0;
+        record_.number = 0;
+        record_.module = 0;
+        encoded_size_ = 0;
+        previous_sensor_ = 0;
+        values_array_ = {
+            .length = 0,
+            .itemSize = sizeof(float),
+            .buffer = (void *)&values_,
+        };
+
+        bzero(values_, sizeof(values_));
     }
 
     size_t size_of_encoding(uint8_t module, uint8_t sensor, float value) const {
-        auto size = 0u;
+        size_t size = 0u;
 
         if (values_array_.length == 0) {
-            size += pb_varint_size(MaxReadingsPerPacket);
-            size += sensor > 0 ? pb_varint_size(sensor) + 1 : 0;
-            size += module > 0 ? pb_varint_size(module) + 1 : 0;
-            size += 1; // Tag for values.
+            size += module > 0 ? pb_varint_size(module) + TagSize : 0;
+            size += sensor > 0 ? pb_varint_size(sensor) + TagSize : 0;
+            size += pb_varint_size(MaxReadingsPerPacket) + TagSize;
         }
         else {
             if (previous_sensor_ + 1 != sensor) {
-                size += sensor > 0 ? pb_varint_size(sensor) + 1 : 0;
+                size += sensor > 0 ? pb_varint_size(sensor) + TagSize : 0;
             }
         }
 
@@ -78,6 +88,7 @@ public:
             record_.module = module;
             record_.sensor = sensor;
             record_.values.arg = (void *)&values_array_;
+            previous_sensor_ = sensor;
         }
         else {
             if (previous_sensor_ + 1 != sensor) {
@@ -87,11 +98,13 @@ public:
 
         FK_ASSERT(record_.module == module);
 
-        values_[values_array_.length++] = value;
+        values_[values_array_.length] = value;
+
+        auto size = size_of_encoding(module, sensor, value);
 
         previous_sensor_ = sensor;
-
-        encoded_size_ += size_of_encoding(module, sensor, value);
+        encoded_size_ += size;
+        values_array_.length++;
     }
 
     EncodedMessage *encode(Pool &pool) {
@@ -100,7 +113,7 @@ public:
         }
         auto encoded = pool.encode(fk_data_LoraRecord_fields, &record_, false);
         if (encoded->size != encoded_size_) {
-            logwarn("encoded size differs from predicted (%zd != %zd)", encoded->size, encoded_size_);
+            FK_ASSERT(encoded->size == encoded_size_);
         }
         return encoded;
     }
@@ -138,20 +151,19 @@ tl::expected<EncodedMessage*, Error> LoraPacketizer::packetize(TakenReadings con
             for (auto s = 0u; s < module.readings->size(); ++s) {
                 auto value = module.readings->get(s);
                 if (value != 0.0f) {
-                    if (record.encoded_size() + record.size_of_encoding(module.position, s, value) >= maximum_packet_size_) {
+                    auto adding = record.size_of_encoding(module.position, s, value);
+                    if (record.encoded_size() + adding >= maximum_packet_size_) {
                         append(&head, &tail, record.encode(pool));
-
-                        record = { pool };
+                        record.clear();
                     }
 
-                    logdebug("reading: %d/%d %f", module.position, s, value);
                     record.write_reading(module.position, s, value);
+                    logdebug("reading: %d/%d %f (%zd)", module.position, s, value, record.encoded_size());
                 }
             }
 
             append(&head, &tail, record.encode(pool));
-
-            record = { pool };
+            record.clear();
         }
     }
 
--- tests/hosted/lora_packetizer_tests.cpp
+++ tests/hosted/lora_packetizer_tests.cpp
@@ -110,29 +110,27 @@ TEST_F(LoraPacketizerSuite, TwoModulesMultipleReadings) {
     }
 }
 
-TEST_F(LoraPacketizerSuite, MultipleModulesVerifyLength) {
+TEST_F(LoraPacketizerSuite, MultipleModulesVerifyLength1) {
     StaticPool<1024> pool("Pool");
 
-    // auto module_readings0 = new (pool) NModuleReadings<0>();
-    // auto module_readings1 = new (pool) NModuleReadings<0>();
-    auto module_readings2 = new (pool) NModuleReadings<3>();
-    module_readings2->set(0, 1725.000000);
-    module_readings2->set(1, 1726.000000);
-    module_readings2->set(2, 1727.000000);
+    auto module_readings0 = new (pool) NModuleReadings<3>();
+    module_readings0->set(0, 1725.000000);
+    module_readings0->set(1, 1726.000000);
+    module_readings0->set(2, 1727.000000);
 
     auto i = 0u;
-    auto module_readings3 = new (pool) NModuleReadings<13>();
-    module_readings3->set(i++, 24.196228);
-    module_readings3->set(i++, 100.226753);
-    module_readings3->set(i++, 22.937500);
-    module_readings3->set(i++, 7.200000);
-    module_readings3->set(i++, 0.000000);
-    module_readings3->set(i++, 157.000000);
-    module_readings3->set(i++, 309.375000);
-    module_readings3->set(i++, 9.600000);
-    module_readings3->set(i++, 9.600000);
-    module_readings3->set(i++, 8.067230);
-    module_readings3->set(i++, 147.000000);
+    auto module_readings1 = new (pool) NModuleReadings<13>();
+    module_readings1->set(i++, 24.196228);
+    module_readings1->set(i++, 100.226753);
+    module_readings1->set(i++, 22.937500);
+    module_readings1->set(i++, 7.200000);
+    module_readings1->set(i++, 0.000000);
+    module_readings1->set(i++, 157.000000);
+    module_readings1->set(i++, 309.375000);
+    module_readings1->set(i++, 9.600000);
+    module_readings1->set(i++, 9.600000);
+    module_readings1->set(i++, 8.067230);
+    module_readings1->set(i++, 147.000000);
 
     ModuleReadingsCollection all_readings{ pool };
     all_readings.emplace_back(ModuleMetaAndReadings{
@@ -140,7 +138,7 @@ TEST_F(LoraPacketizerSuite, MultipleModulesVerifyLength) {
         .id = nullptr,
         .meta = &fk_test_module_fake_empty,
         .sensors = nullptr,
-        .readings = module_readings2,
+        .readings = module_readings0,
     });
 
     all_readings.emplace_back(ModuleMetaAndReadings{
@@ -148,7 +146,7 @@ TEST_F(LoraPacketizerSuite, MultipleModulesVerifyLength) {
         .id = nullptr,
         .meta = &fk_test_module_fake_empty,
         .sensors = nullptr,
-        .readings = module_readings3,
+        .readings = module_readings1,
     });
 
     LoraPacketizer packetizer;
@@ -160,3 +158,75 @@ TEST_F(LoraPacketizerSuite, MultipleModulesVerifyLength) {
         fk_dump_memory("packet ", p->buffer, p->size);
     }
 }
+
+TEST_F(LoraPacketizerSuite, MultipleModulesVerifyLength2) {
+    StaticPool<1024> pool("Pool");
+
+    // 00027444 lora       debug   packetizer: begin time=7435 reading=0
+    // 00027444 lora       debug   packetizer: reading: 2/0 1726.000000
+    // 00027444 lora       debug   packetizer: reading: 2/1 1727.000000
+    // 00027444 lora       debug   packetizer: reading: 2/2 1728.000000
+    // PTR 00 c0 d7 44 00 e0 d7 44 00 00 d8 44  (12 bytes)
+    // PTR 00 c0 d7 44 00 e0 d7 44 00 00 d8 44  (12 bytes)
+    // 00027445 lora       warn    packetizer: encoded size differs from predicted (19 != 21)
+    // LORA 10 8b 3a 20 02 32 0c 00 c0 d7 44 00 e0 d7 44 00 00 d8 44  (19 bytes)
+
+    auto module_readings0 = new (pool) NModuleReadings<3>();
+    module_readings0->set(0, 1726.000000);
+    module_readings0->set(1, 1727.000000);
+    module_readings0->set(2, 1728.000000);
+
+    ModuleReadingsCollection all_readings{ pool };
+    all_readings.emplace_back(ModuleMetaAndReadings{
+        .position = 2,
+        .id = nullptr,
+        .meta = &fk_test_module_fake_empty,
+        .sensors = nullptr,
+        .readings = module_readings0,
+    });
+
+    LoraPacketizer packetizer;
+    auto taken = TakenReadings{ 7435, 0, all_readings };
+    auto packets = packetizer.packetize(taken, pool);
+    ASSERT_TRUE(packets);
+
+    for (auto p = *packets; p != nullptr; p = p->link) {
+        fk_dump_memory("packet ", p->buffer, p->size);
+    }
+}
+
+/*
+00055214 lora       debug   packetizer: begin time=25531 reading=7350
+00055214 lora       debug   packetizer: reading: 2/0 1726.000000
+00055214 lora       debug   packetizer: reading: 2/1 1726.000000
+00055214 lora       debug   packetizer: reading: 2/2 1731.000000
+PTR 00 c0 d7 44 00 c0 d7 44 00 60 d8 44  (12 bytes)
+PTR 00 c0 d7 44 00 c0 d7 44 00 60 d8 44  (12 bytes)
+00055216 lora       debug   packetizer: reading: 6/0 45.546654
+00055216 lora       debug   packetizer: reading: 6/1 23.531322
+00055216 lora       debug   packetizer: reading: 6/2 100.158249
+00055216 lora       debug   packetizer: reading: 6/3 22.250000
+PTR c6 2f 36 42 26 40 bc 41 06 51 c8 42 00 00 b2 41  (16 bytes)
+PTR c6 2f 36 42 26 40 bc 41 06 51 c8 42 00 00 b2 41  (16 bytes)
+00055217 lora       warn    packetizer: encoded size differs from predicted (20 != 22)
+00055217 lora       debug   packetizer: reading: 6/5 9.600000
+00055217 lora       debug   packetizer: reading: 6/6 337.000000
+00055217 lora       debug   packetizer: reading: 6/7 1830.468750
+PTR 9a 99 19 41 00 80 a8 43 00 cf e4 44  (12 bytes)
+PTR 9a 99 19 41 00 80 a8 43 00 cf e4 44  (12 bytes)
+00055218 lora       debug   packetizer: reading: 6/8 9.600000
+00055218 lora       debug   packetizer: reading: 6/9 202.000000
+00055218 lora       debug   packetizer: reading: 6/10 9.600000
+PTR 9a 99 19 41 00 00 4a 43 9a 99 19 41  (12 bytes)
+PTR 9a 99 19 41 00 00 4a 43 9a 99 19 41  (12 bytes)
+00055219 lora       debug   packetizer: reading: 6/11 202.000000
+00055219 lora       debug   packetizer: reading: 6/12 7.600000
+00055219 lora       debug   packetizer: reading: 6/13 312.000000
+PTR 00 00 4a 43 33 33 f3 40 00 00 9c 43  (12 bytes)
+PTR 00 00 4a 43 33 33 f3 40 00 00 9c 43  (12 bytes)
+LORA 10 bb c7 01 18 b6 39 20 02 32 0c 00 c0 d7 44 00 c0 d7 44 00 60 d8 44  (23 bytes)
+LORA 20 06 32 10 c6 2f 36 42 26 40 bc 41 06 51 c8 42 00 00 b2 41  (20 bytes)
+LORA 20 06 28 05 32 0c 9a 99 19 41 00 80 a8 43 00 cf e4 44  (18 bytes)
+LORA 20 06 28 08 32 0c 9a 99 19 41 00 00 4a 43 9a 99 19 41  (18 bytes)
+LORA 20 06 28 0b 32 0c 00 00 4a 43 33 33 f3 40 00 00 9c 43  (18 bytes)
+*/
