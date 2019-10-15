12f7d29d816706e66f76ec89c8783062b6bd8688
Jacob Lewallen <jlewallen@gmail.com>
2019-10-14 19:58:42 -0700

    Include core temperature in diagnostics module.

--- modules/diagnostics/main/diagnostics_module.cpp
+++ modules/diagnostics/main/diagnostics_module.cpp
@@ -1,6 +1,7 @@
 #include "diagnostics_module.h"
 #include "platform.h"
 #include "state.h"
+#include "temperature.h"
 
 using namespace fk;
 
@@ -9,6 +10,7 @@ static SensorMetadata const fk_module_sensor_metas[] = {
     { .name = "battery_voltage", .unitOfMeasure = "v",     .flags = 0 },
     { .name = "free_memory",     .unitOfMeasure = "bytes", .flags = 0 },
     { .name = "uptime",          .unitOfMeasure = "ms",    .flags = 0 },
+    { .name = "temperature",     .unitOfMeasure = "C",     .flags = 0 },
 };
 
 static ModuleSensors const fk_module_sensors = {
@@ -25,10 +27,18 @@ ModuleSensors const *DiagnosticsModule::get_sensors(Pool &pool) {
 }
 
 ModuleReadings *DiagnosticsModule::take_readings(ModuleContext mc, fk::Pool &pool) {
-    auto mr = new(pool) NModuleReadings<4>();
+    CoreTemperature core_temperature_sensor{ get_board()->i2c_core() };
+
+    auto mr = new(pool) NModuleReadings<5>();
     mr->set(0, mc.gs()->power.charge);
     mr->set(1, mc.gs()->power.voltage);
     mr->set(2, fk_free_memory());
     mr->set(3, fk_uptime());
+
+    float core_temperature = 0.0f;
+    if (core_temperature_sensor.read(&core_temperature)) {
+        mr->set(4, core_temperature);
+    }
+
     return mr;
 }
