028796c69ef07ce7cb37f19e314580e93cb9759b
Jacob Lewallen <jlewallen@gmail.com>
2019-10-14 22:33:15 -0700

    Try and make logging opening and writing more resilient.

--- fk/hal/metal/metal_sd_card.cpp
+++ fk/hal/metal/metal_sd_card.cpp
@@ -20,7 +20,6 @@ static SdFat sd(&SD_SPI);
 static SdFile log_file;
 static char log_file_name[13] = LOG_FILE_BASE_NAME "000.txt";
 static bool log_initialized{ false };
-static bool log_ready{ false };
 
 MetalSdCard::MetalSdCard() {
 }
@@ -50,41 +49,37 @@ bool MetalSdCard::append_logs(circular_buffer<char> &buffer) {
     auto started = fk_uptime();
 
     if (!begin()) {
-        log_initialized = false;
-        log_ready = false;
         if (sd.card()->errorCode()) {
             loginfo("ignoring logs: no sd card, error = 0x%x", sd.card()->errorCode());
         }
         return false;
     }
-
-    if (!log_initialized) {
-        for (auto counter = 0; counter < 1000; ++counter) {
-            tiny_snprintf(log_file_name, sizeof(log_file_name), "%s%03d.txt", LOG_FILE_BASE_NAME, counter);
-            if (!sd.exists(log_file_name)) {
-                break;
+    else {
+        if (!log_initialized) {
+            for (auto counter = 0; counter < 1000; ++counter) {
+                tiny_snprintf(log_file_name, sizeof(log_file_name), "%s%03d.txt", LOG_FILE_BASE_NAME, counter);
+                if (!sd.exists(log_file_name)) {
+                    log_initialized = true;
+                    break;
+                }
             }
         }
+    }
 
+    auto size = buffer.size();
+    if (log_initialized) {
         if (!log_file.open(log_file_name, O_WRONLY | O_CREAT | O_EXCL)) {
             logerror("error opening %s", log_file_name);
             return false;
         }
 
-        loginfo("opened %s", log_file_name);
-        log_initialized = true;
-        log_ready = true;
-    }
-
-    auto size = buffer.size();
-
-    if (log_ready) {
         log_file.write(buffer.buffer(), size);
         log_file.flush();
         loginfo("flushed %d to %s (%" PRIu32 "ms) (%" PRIu32 " bytes)", size, log_file_name, fk_uptime() - started, log_file.fileSize());
     }
     else {
         loginfo("ignored %d (%" PRIu32 "ms)", size, fk_uptime() - started);
+        log_initialized = false;
     }
 
     return true;
