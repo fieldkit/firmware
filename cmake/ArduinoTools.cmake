0470fd0616f323c744bd62060c8203e418510634
Jacob Lewallen <jlewallen@gmail.com>
2019-10-22 09:07:35 -0700

    mkfirmware will generate the binary now.

--- cmake/ArduinoTools.cmake
+++ cmake/ArduinoTools.cmake
@@ -17,7 +17,7 @@ function(enable_fkb_firmware target)
   add_custom_command(
     OUTPUT ${fkb_elf_file}
     DEPENDS ${elf_file}
-    COMMAND ${loading_PATH}/tools/mkfirmware.py --elf ${elf_file} --fkb ${fkb_elf_file}
+    COMMAND ${loading_PATH}/tools/mkfirmware.py --elf ${elf_file} --fkb ${fkb_elf_file} --bin ${fkb_bin_file}
     WORKING_DIRECTORY ${binary_dir}
     )
 
@@ -25,18 +25,6 @@ function(enable_fkb_firmware target)
   add_dependencies(${fkb_elf_target} ${elf_target})
 
   set_target_properties(${fkb_elf_target} PROPERTIES OUTPUT_NAME ${fkb_elf_file})
-
-  add_custom_command(
-    OUTPUT ${fkb_bin_file}
-    DEPENDS ${fkb_elf_file}
-    COMMAND ${ARDUINO_OBJCOPY} -O binary ${fkb_elf_file} ${fkb_bin_file}
-    WORKING_DIRECTORY ${binary_dir}
-    )
-
-  add_custom_target(${fkb_bin_target} ALL DEPENDS ${fkb_bin_file})
-  add_dependencies(${fkb_bin_target} ${fkb_elf_target})
-
-  set_target_properties(${fkb_bin_target} PROPERTIES OUTPUT_NAME ${fkb_bin_file})
 
   set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES
     "${CMAKE_CURRENT_BINARY_DIR}/${target}.syms"
