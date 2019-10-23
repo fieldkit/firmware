4feb5caa78b16b7e50c508e0e1b4ffcad78cb3fb
Jacob Lewallen <jlewallen@gmail.com>
2019-10-23 13:00:15 -0700

    These checks moved into the os.c code.

--- fk/status_logging.cpp
+++ fk/status_logging.cpp
@@ -11,35 +11,6 @@ namespace fk {
 
 FK_DECLARE_LOGGER("status");
 
-/**
- * This can go away when we're sure this holds. Basically, if we're
- * running then every task of higher priority should be in WAIT,
- * FINISHED, SUSPENDED, or PANIC... basically anything other than
- * IDLE, which would mean that it's waiting for a turn to run and
- * could possibly be in the middle of something.
- */
-static void verify_tasks_priority() __attribute__((unused)) ;
-static void verify_tasks_priority() {
-    __disable_irq();
-    for (auto iter = osg.tasks; iter != nullptr; iter = iter->np) {
-        if (iter->priority > os_task_self()->priority) {
-            if (iter->status != OS_TASK_STATUS_FINISHED && iter->status != OS_TASK_STATUS_WAIT && iter->status != OS_TASK_STATUS_SUSPENDED && iter->status != OS_TASK_STATUS_PANIC) {
-                logerror("%s has status %s", iter->name, os_task_status_str(iter->status));
-            }
-        }
-    }
-    __enable_irq();
-}
-
-static void log_task_info() __attribute__((unused));
-static void log_task_info() {
-    __disable_irq();
-    for (auto iter = osg.tasks; iter != nullptr; iter = iter->np) {
-        logverbose("task: %10s %-30s %" PRIu32 "us", iter->name, os_task_status_str(iter->status), iter->runtime);
-    }
-    __enable_irq();
-}
-
 static void log_status() {
     auto gs = try_get_global_state_ro();
     if (!gs) {
@@ -58,7 +29,6 @@ static void log_status() {
 }
 
 void fk_status_log() {
-    verify_tasks_priority();
     log_status();
 }
 
