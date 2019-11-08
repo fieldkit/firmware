aa699230d40ae274d2707234b927f00ffdb7eee0
Jacob Lewallen <jlewallen@gmail.com>
2019-11-08 09:30:39 -0800

    Show devaddr from LoRa module.

--- fk/hal/metal/metal_lora.cpp
+++ fk/hal/metal/metal_lora.cpp
@@ -14,6 +14,7 @@ Rn2903LoraNetwork::Rn2903LoraNetwork() {
 
 bool Rn2903LoraNetwork::query_status() {
     const char *line = nullptr;
+
     if (!rn2903_.simple_query("sys get hweui", &line, 1000)) {
         return false;
     }
@@ -23,12 +24,18 @@ bool Rn2903LoraNetwork::query_status() {
     if (!rn2903_.simple_query("mac get appeui", &line, 1000)) {
         return false;
     }
+
     if (!rn2903_.simple_query("mac get deveui", &line, 1000)) {
         return false;
     }
-
     FK_ASSERT(hex_string_to_bytes(device_eui_, sizeof(device_eui_), line) == sizeof(device_eui_));
 
+    if (!rn2903_.simple_query("mac get devaddr", &line, 1000)) {
+        return false;
+    }
+    if (!rn2903_.simple_query("mac get upctr", &line, 1000)) {
+        return false;
+    }
     if (!rn2903_.simple_query("mac get dr", &line, 1000)) {
         return false;
     }
@@ -46,7 +53,11 @@ bool Rn2903LoraNetwork::query_status() {
 }
 
 bool Rn2903LoraNetwork::join(const char *app_eui, const char *app_key, int32_t retries, uint32_t retry_delay) {
-    return rn2903_.join(app_eui, app_key, retries, retry_delay);
+    if (!rn2903_.join(app_eui, app_key, retries, retry_delay)) {
+        return false;
+    }
+
+    return true;
 }
 
 bool Rn2903LoraNetwork::power(bool on) {
