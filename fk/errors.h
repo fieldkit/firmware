6cf22d5c7e50b33598ba13f5f13eda402894415a
Jacob Lewallen <jlewallen@gmail.com>
2019-11-11 10:37:38 -0800

    Make errors negative.

--- fk/errors.h
+++ fk/errors.h
@@ -2,13 +2,13 @@
 
 namespace fk {
 
-enum class Error {
-    None,
-    General,
-    Bus,
-    TooSoon,
-    IO,
-    EoF,
+enum class Error : int32_t {
+    None = 0,
+    General = -1,
+    Bus = -2,
+    TooSoon = -3,
+    IO = -4,
+    EoF = -5,
 };
 
 }
