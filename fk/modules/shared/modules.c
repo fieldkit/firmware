9a11ea5e69443d386774d67fc2dd21b8f8cd90eb
Jacob Lewallen <jlewallen@gmail.com>
2020-01-14 09:40:43 -0800

    Ignore backplane led controller.

--- fk/modules/shared/modules.c
+++ fk/modules/shared/modules.c
@@ -11,11 +11,13 @@ uint32_t fk_module_header_valid(ModuleHeader const *header) {
 
 uint8_t fk_module_is_available_i2c_address(uint8_t address) {
     switch (address) {
-    case 0x20: return 0;
-    case 0x4d: return 0;
-    case 0x50: return 0;
-    case 0x58: return 0;
-    case 0x70: return 0;
+    case 0x20: return 0; // backplane mcp23008
+    case 0x4d: return 0; // TODO: ?
+    case 0x50: return 0; // module eeprom
+    case 0x58: return 0; // module eeprom
+    case 0x70: return 0; // backplane tca9548a
+    case 0x14: return 0; // backplane led driver
+    case 0x0c: return 0; // TODO: ?
     default: return 1;
     }
 }
