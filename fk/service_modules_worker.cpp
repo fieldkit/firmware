40a6c134c2420819cab17891aa62b9ad911df084
Jacob Lewallen <jlewallen@gmail.com>
2020-01-17 15:36:15 -0800

    Lock order change.

--- fk/service_modules_worker.cpp
+++ fk/service_modules_worker.cpp
@@ -11,10 +11,10 @@ ServiceModulesWorker::ServiceModulesWorker() {
 }
 
 void ServiceModulesWorker::run(Pool &pool) {
-    auto gs = get_global_state_ro();
-    auto lock = storage_mutex.acquire(UINT32_MAX);
     auto mm = get_modmux();
+    auto lock = storage_mutex.acquire(UINT32_MAX);
     auto modules_lock = mm->lock();
+    auto gs = get_global_state_ro();
     auto modules_bus = get_board()->i2c_module();
 
     ScanningContext ctx{ mm, gs.get(), modules_bus };
