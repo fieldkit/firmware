a80263f1ae45428325baacf7f51af589b9d643f5
Jacob Lewallen <jlewallen@gmail.com>
2020-01-20 14:51:55 -0800

    Fix turning on display on topology change.

--- fk/tasks/scheduler.cpp
+++ fk/tasks/scheduler.cpp
@@ -62,9 +62,9 @@ void task_handler_scheduler(void *params) {
                 if (has_module_topology_changed(topology)) {
                     loginfo("topology changed: [%s]", topology.string());
                     get_ipc()->launch_worker(create_pool_worker<ScanModulesWorker>());
+                    fk_start_task_if_necessary(&display_task);
                 }
                 check_for_modules_time = fk_uptime() + OneSecondMs;
-                fk_start_task_if_necessary(&display_task);
             }
 
             if (check_battery_time < fk_uptime()) {
