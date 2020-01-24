b65114713d37900617b6a41611577ba2a90566ae
Jacob Lewallen <jlewallen@gmail.com>
2020-01-24 08:44:11 -0800

    Log actual bytes left in the stack.

--- fk/tasks/worker.cpp
+++ fk/tasks/worker.cpp
@@ -16,9 +16,15 @@ void task_handler_worker(void *params) {
     worker->run();
     delete worker;
 
-    auto highwater = os_task_highwater(os_task_self());
+    // Highwater is kind of a nisnomer because it's the number of
+    // dwords still remaining on the stack. So we're gonna tell the
+    // user how much was left.
+    auto self = os_task_self();
+    auto bytes_left = os_task_highwater(self) * sizeof(uint32_t);
+    auto stack_percentage = bytes_left / (float)self->stack_size * 100.0f;
 
-    loginfo("done (%" PRIu32 "ms) highwater = (%" PRIu32 ")", fk_uptime() - started, highwater);
+    loginfo("done (%" PRIu32 "ms) stack: %" PRIu32 " / %" PRIu32 " bytes %.2f%% free)",
+            fk_uptime() - started, self->stack_size, bytes_left, stack_percentage);
 }
 
 }
