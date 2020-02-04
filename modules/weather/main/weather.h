ecf357ece2c22e6e81778e37531750642d6ed0cc
Jacob Lewallen <jlewallen@gmail.com>
2020-02-04 14:42:02 -0800

    Start of a struct for keeping less granular weather data.

--- modules/weather/main/weather.h
+++ modules/weather/main/weather.h
@@ -15,6 +15,9 @@ typedef struct fk_rain_t {
     uint32_t ticks;
 } fk_rain_t;
 
+/**
+ * This accumulates one second of weather information.
+ */
 typedef struct fk_weather_t {
     uint32_t seconds;
 
@@ -42,6 +45,32 @@ typedef struct fk_weather_config_t {
     uint16_t reserved[3];
     uint32_t crc;
 } fk_weather_config_t;
+
+/**
+ * This accumulates one minute of weather information.
+ */
+typedef struct fk_weather_minute_t {
+    uint32_t minute;
+
+    uint32_t humidity;
+    uint32_t temperature_1;
+
+    uint32_t pressure;
+    uint32_t temperature_2;
+
+    fk_wind_t wind[60];
+    fk_rain_t rain[60];
+
+    uint32_t crc;
+} fk_weather_minute_t;
+
+typedef struct fk_eeprom_record_t {
+    uint8_t type;
+
+    union {
+        fk_weather_minute_t weather;
+    } r;
+} fk_eeprom_record_t;
 
 #define FK_WEATHER_ERROR_SENSORS_STARTUP               (0x1)
 #define FK_WEATHER_ERROR_SENSORS_READING               (0x2)
