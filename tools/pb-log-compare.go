5a8d6ff817307779b0d976727c361f9993df750e
Jacob Lewallen <jlewallen@gmail.com>
2020-02-03 17:07:15 -0800

    Work on the binary compare tool.

--- tools/pb-log-compare.go
+++ tools/pb-log-compare.go
@@ -11,16 +11,20 @@ import (
 
 	"github.com/r3labs/diff"
 
+	"github.com/k0kubun/pp"
+
 	"github.com/golang/protobuf/proto"
 
-	pb "github.com/fieldkit/data-protocol"
+	pbapp "github.com/fieldkit/app-protocol"
+	pbdata "github.com/fieldkit/data-protocol"
 )
 
 type options struct {
+	Delimited bool
 	Lora      bool
 	Data      bool
 	Signed    bool
-	Delimited bool
+	HttpReply bool
 }
 
 func main() {
@@ -30,6 +34,7 @@ func main() {
 	flag.BoolVar(&o.Lora, "lora", false, "")
 	flag.BoolVar(&o.Data, "data", false, "")
 	flag.BoolVar(&o.Signed, "signed", false, "")
+	flag.BoolVar(&o.HttpReply, "http-reply", false, "")
 
 	flag.Parse()
 
@@ -38,6 +43,8 @@ func main() {
 	blocks := make(map[string]string)
 
 	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Printf("%+v\n", o)
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -51,9 +58,12 @@ func main() {
 		}
 	}
 
-	dataRecords := make([]*pb.DataRecord, 0)
-	signedRecords := make([]*pb.SignedRecord, 0)
-	loraRecords := make([]*pb.LoraRecord, 0)
+	fmt.Printf("blocks: %d\n", len(blocks))
+
+	dataRecords := make([]*pbdata.DataRecord, 0)
+	signedRecords := make([]*pbdata.SignedRecord, 0)
+	loraRecords := make([]*pbdata.LoraRecord, 0)
+	httpReplies := make([]*pbapp.HttpReply, 0)
 
 	for key, value := range blocks {
 		bytes, err := hex.DecodeString(value)
@@ -71,35 +81,50 @@ func main() {
 		}
 
 		if o.Data {
-			var dataRecord pb.DataRecord
+			var dataRecord pbdata.DataRecord
 			err = buffer.Unmarshal(&dataRecord)
 			if err == nil {
-				fmt.Printf("%+v\n", dataRecord)
+				pp.Print(dataRecord)
+				fmt.Println()
 				dataRecords = append(dataRecords, &dataRecord)
-			} else if o.Data {
-				fmt.Printf("%v\n", err)
+			} else {
+				fmt.Printf("error parsing data: %v\n", err)
 			}
 		}
 
 		if o.Signed {
-			var signedRecord pb.SignedRecord
+			var signedRecord pbdata.SignedRecord
 			err = buffer.Unmarshal(&signedRecord)
 			if err == nil {
-				fmt.Printf("%+v\n", signedRecord)
+				pp.Print(signedRecord)
+				fmt.Println()
 				signedRecords = append(signedRecords, &signedRecord)
-			} else if o.Signed {
-				fmt.Printf("%v\n", err)
+			} else {
+				fmt.Printf("%error parsing signed: v\n", err)
 			}
 		}
 
 		if o.Lora {
-			var loraRecord pb.LoraRecord
+			var loraRecord pbdata.LoraRecord
 			err = buffer.Unmarshal(&loraRecord)
 			if err == nil {
-				fmt.Printf("%+v\n", loraRecord)
+				pp.Print(loraRecord)
+				fmt.Println()
 				loraRecords = append(loraRecords, &loraRecord)
-			} else if o.Lora {
-				fmt.Printf("%v\n", err)
+			} else {
+				fmt.Printf("error parsing lora: %v\n", err)
+			}
+		}
+
+		if o.HttpReply {
+			var httpReply pbapp.HttpReply
+			err = buffer.Unmarshal(&httpReply)
+			if err == nil {
+				pp.Print(httpReply)
+				fmt.Println()
+				httpReplies = append(httpReplies, &httpReply)
+			} else {
+				fmt.Printf("error parsing app: %v\n", err)
 			}
 		}
 
@@ -109,6 +134,19 @@ func main() {
 
 	if len(dataRecords) == 2 {
 		changelog, err := diff.Diff(dataRecords[0], dataRecords[1])
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println()
+
+		for _, change := range changelog {
+			fmt.Printf("%+v\n", change)
+		}
+	}
+
+	if len(httpReplies) == 2 {
+		changelog, err := diff.Diff(httpReplies[0], httpReplies[1])
 		if err != nil {
 			panic(err)
 		}
