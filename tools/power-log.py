6785a936f8492ca858d7ef000f04e3085deb56db
Jacob Lewallen <jlewallen@gmail.com>
2020-02-10 15:12:14 -0800

    Just save logs and power information, do analysis offline.

--- tools/power-log.py
+++ tools/power-log.py
@@ -18,262 +18,62 @@ class Database:
         self.queue = queue.Queue()
 
     def open(self):
-        self.run = 0
         self.thread = threading.Thread(target=self.inserter, args=())
         self.thread.daemon = True
         self.thread.start()
 
-    def started(self):
-        self.run += 1
-
     def inserter(self):
         self.db = sqlite3.connect('power.db')
-
-        c = self.db.cursor()
-
-        c.execute('CREATE TABLE IF NOT EXISTS raw ('
-                  '  id INTEGER PRIMARY KEY AUTOINCREMENT,'
-                  '  time TIMESTAMP NOT NULL,'
-                  '  energy NUMERIC NOT NULL,'
-                  '  message TEXT NOT NULL'
-                  ')')
-
-        c.execute('CREATE TABLE IF NOT EXISTS samples ('
-                  '  id INTEGER PRIMARY KEY AUTOINCREMENT,'
-                  '  time TIMESTAMP NOT NULL,'
-                  '  run INTEGER NOT NULL,'
-                  '  device_id TEXT NOT NULL,'
-                  '  firmware_hash TEXT NOT NULL,'
-                  '  uptime INTEGER NOT NULL,'
-                  '  modules TEXT,'
-                  '  status BOOLEAN NOT NULL,'
-                  '  tasks TEXT NOT NULL,'
-                  '  task_start BOOLEAN,'
-                  '  task_name TEXT,'
-                  '  task_duration INTEGER,'
-                  '  energy_delta NUMERIC,'
-                  '  energy_total NUMERIC NOT NULL'
-                  ')')
-
-        run = c.execute("SELECT MAX(run) FROM samples").fetchone()[0]
-        if run: self.run = run
-
+        self.db.cursor().execute('CREATE TABLE IF NOT EXISTS raw ('
+                                '  id INTEGER PRIMARY KEY AUTOINCREMENT,'
+                                '  time TIMESTAMP NOT NULL,'
+                                '  energy NUMERIC NOT NULL,'
+                                '  message TEXT NOT NULL'
+                                ')')
         self.db.commit()
 
-        while True:
-            while not self.queue.empty():
-                sql, values = self.queue.get()
-                self.db.cursor().execute(sql, values)
-                self.db.commit()
+        pending = 0
 
-            time.sleep(1)
+        while True:
+            try:
+                sql, values = self.queue.get(True, 0.5)
+                self.db.cursor().execute(sql, values)
+                pending += 1
+            except queue.Empty:
+                if pending > 0:
+                    self.db.commit()
+                    pending = 0
+            except:
+                time.sleep(1)
 
     def add_raw(self, energy, message):
         values = [ time.time(), energy, message ]
         self.queue.put(('INSERT INTO raw (time, energy, message) VALUES (?, ?, ?)', values))
 
-    def add_sample(self, sample):
-        if sample is None:
-            return
-        values = [
-            time.time(),
-            self.run,
-            sample.device_id,
-            sample.firmware_hash,
-            sample.uptime,
-            sample.tasks,
-            sample.modules,
-            sample.status,
-            sample.task_start,
-            sample.task_name,
-            sample.task_duration,
-            sample.energy_delta,
-            sample.energy_total,
-        ]
-        self.queue.put(('INSERT INTO samples (time, run, device_id, firmware_hash, uptime, tasks, modules, status, task_start, task_name, task_duration, energy_delta, energy_total) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', values))
-
-class Sample:
-    def __init__(self):
-        self.device_id = None
-        self.firmware_hash = None
-        self.uptime = None
-        self.modules = ''
-        self.status = False
-        self.tasks = ''
-        self.task_start = None
-        self.task_name = None
-        self.task_duration = None
-        self.energy_delta = None
-        self.energy_total = 0.0
-
-    def make_status(self, delta):
-        c = Sample()
-        c.device_id = self.device_id
-        c.firmware_hash = self.firmware_hash
-        c.uptime = self.uptime
-        c.modules = None
-        c.status = True
-        c.tasks = self.tasks
-        c.task_start = False
-        c.task_name = None
-        c.task_duration = None
-        c.energy_delta = delta
-        c.energy_total = self.energy_total
-        return c
-
-    def make_started(self, name):
-        c = Sample()
-        c.device_id = self.device_id
-        c.firmware_hash = self.firmware_hash
-        c.uptime = self.uptime
-        c.modules = None
-        c.status = False
-        c.tasks = self.tasks
-        c.task_start = True
-        c.task_name = name
-        c.task_duration = None
-        c.energy_delta = None
-        c.energy_total = self.energy_total
-        return c
-
-    def make_finished(self, name, delta, duration):
-        c = Sample()
-        c.device_id = self.device_id
-        c.firmware_hash = self.firmware_hash
-        c.uptime = self.uptime
-        c.modules = self.modules
-        c.status = False
-        c.tasks = self.tasks
-        c.task_start = False
-        c.task_name = name
-        c.task_duration = duration
-        c.energy_delta = delta
-        c.energy_total = self.energy_total
-        return c
-
-class EnergyAndTime:
-    def __init__(self, energy, time):
-        self.energy = energy
-        self.time = time
-
 class FkListener:
     def __init__(self, args):
         self.db = Database()
         self.db.open()
-        self.energy_base = 0.0
-        self.tasks = []
-        self.modules = []
-        self.device_time_queue = queue.Queue()
-        self.device_time = None
-        self.active = {}
-        self.sample = Sample()
-        self.status_energy = None
-
-    def update_energy(self, energy):
-        self.sample.energy_total = energy - self.energy_base
-
-    def message(self, message):
-        if self.sample.energy_total:
-            self.db.add_raw(self.sample.energy_total, message)
+        self.energy = None
 
     def get_device_time(self):
-        while not self.device_time_queue.empty():
-            ignored = self.device_time_queue.get()
-        return self.device_time_queue.get()
+        return 0
 
-    def update_device_time(self, time):
-        self.device_time = time
-        self.device_time_queue.put(time)
-        self.sample.uptime = time
+    def update_energy(self, energy):
+        self.energy = energy
 
-    def started(self):
-        self.energy_base = self.sample.energy_total
-        self.tasks = []
-        self.sample.tasks = ''
-        self.db.started()
-        logging.info("STARTED")
-
-    def serial(self, serial):
-        self.sample.device_id = serial
-        logging.info("SERIAL: %s" % (self.sample.device_id,))
-
-    def firmware(self, firmware_hash):
-        self.sample.firmware_hash = firmware_hash
-        logging.info("FIRMWARE: %s" % (self.sample.firmware_hash,))
-
-    def task_started(self, name):
-        if name == 'readings':
-            self.modules = []
-        self.active[name] = EnergyAndTime(self.sample.energy_total, self.device_time)
-        self.tasks.append(name)
-        self.sample.tasks = ','.join(self.tasks)
-        self.db.add_sample(self.sample.make_started(name))
-        logging.info("TASKS: %s" % (self.tasks,))
-
-    def task_finished(self, name):
-        a = self.active[name]
-        delta = self.sample.energy_total - a.energy
-        duration = self.device_time - a.time
-        self.active[name] = None
-        self.tasks.remove(name)
-        self.sample.tasks = ','.join(self.tasks)
-        self.sample.modules = ','.join(self.modules)
-        self.db.add_sample(self.sample.make_finished(name, delta, duration))
-        logging.info("TASKS: %s" % (self.tasks,))
-
-    def status(self, name, ip):
-        if self.status_energy:
-            delta = self.sample.energy_total - self.status_energy
-            self.db.add_sample(self.sample.make_status(delta))
-        else:
-            self.db.add_sample(self.sample.make_status(None))
-        self.status_energy = self.sample.energy_total
-
-    def module(self, name, mk, version):
-        self.modules.append(name)
-
-    def connection_serviced(self, path):
-        pass
+    def message(self, message):
+        if self.energy:
+            self.db.add_raw(self.energy, message)
 
 class FkLogParser:
     def __init__(self, listener, args):
         self.args = args
         self.listener = listener
         self.re_ansi_escape = re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]')
-        self.re_starting = re.compile(r'bl: starting')
-        self.re_hash = re.compile(r'hash=\'(\w+)\'')
-        self.re_serial = re.compile(r'serial=\'([\w-]+)\'')
-        self.re_alive = re.compile(r'(\w+):\s+alive')
-        self.re_dead = re.compile(r'(\w+):\s+dead')
-        self.re_routing = re.compile(r'routing \'(.+)\'')
-        self.re_status = re.compile(r'\'(.+)\' \((.+)\) memory')
-        self.re_module = re.compile(r'\'(.+)\' mk=(....) version=(\d+)')
 
     def handle(self, line):
         clean = self.re_ansi_escape.sub('', line).strip()
-        try:
-            parts = clean.split(" ")
-            self.listener.update_device_time(int(parts[0]))
-        except:
-            pass
-
-        m = self.re_starting.search(clean)
-        if m: self.listener.started()
-        m = self.re_hash.search(clean)
-        if m: self.listener.firmware(m.group(1))
-        m = self.re_serial.search(clean)
-        if m: self.listener.serial(m.group(1))
-        m = self.re_alive.search(clean)
-        if m: self.listener.task_started(m.group(1))
-        m = self.re_dead.search(clean)
-        if m: self.listener.task_finished(m.group(1))
-        m = self.re_routing.search(clean)
-        if m: self.listener.connection_serviced(m.group(1))
-        m = self.re_status.search(clean)
-        if m: self.listener.status(m.group(1), m.group(2))
-        m = self.re_module.search(clean)
-        if m: self.listener.module(m.group(1), m.group(2), m.group(3))
-
         self.listener.message(clean)
 
 class Processor:
