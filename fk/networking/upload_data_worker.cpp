0588af9bef6864e18823fba4b64a14a96baa279e
Jacob Lewallen <jlewallen@gmail.com>
2020-02-28 16:27:00 -0800

    Actually open storage and all that jazz.

--- fk/networking/upload_data_worker.cpp
+++ fk/networking/upload_data_worker.cpp
@@ -2,6 +2,9 @@
 #include "networking/http_connection.h"
 #include "state_ref.h"
 #include "utilities.h"
+#include "progress_tracker.h"
+#include "gs_progress_callbacks.h"
+#include "storage/storage.h"
 
 namespace fk {
 
@@ -34,29 +37,59 @@ const char *build_headers(uint32_t first, uint32_t last, uint32_t length, Pool &
 }
 
 void UploadDataWorker::run(Pool &pool) {
-    auto extra_headers = build_headers(0, 0, 0, pool);
+    auto lock = storage_mutex.acquire(UINT32_MAX);
+
+    auto started = fk_uptime();
+    StatisticsMemory memory{ MemoryFactory::get_data_memory() };
+    Storage storage{ &memory, pool };
+
+    if (!storage.begin()) {
+        return;
+    }
+
+    auto file = storage.file(Storage::Meta);
+
+    auto first_block = 0;
+    auto last_block = 0;
+    auto size_info = file.get_size(first_block, last_block, pool);
+    auto upload_length = size_info.size;
+
+    auto extra_headers = build_headers(first_block, last_block, upload_length, pool);
     auto url = "http://192.168.0.100:8080/ingestion";
     auto http = open_http_connection("POST", url, extra_headers, pool);
     if (http == nullptr) {
         return;
     }
 
-    auto buffer = (uint8_t *)pool.malloc(CodeMemoryPageSize);
-    auto total_read = 0u;
+    auto buffer = (uint8_t *)pool.malloc(NetworkBufferSize);
+    auto bytes_copied = 0u;
 
     loginfo("sending...");
 
+    GlobalStateProgressCallbacks gs_progress;
+    auto tracker = ProgressTracker{ &gs_progress, Operation::Download, "download", "", upload_length };
     while (true) {
-        auto reading = CodeMemoryPageSize - 1;
-        auto nread = http->read(buffer, reading);
-        if (nread < 0) {
+        auto to_read = std::min<int32_t>(NetworkBufferSize, upload_length - bytes_copied);
+        auto bytes_read = file.read(buffer, to_read);
+        if (bytes_read != to_read) {
+            logwarn("read error (%" PRId32 " != %" PRId32 ")", bytes_read, to_read);
             break;
         }
 
-        total_read += nread;
+        auto bytes_wrote = http->write(buffer, to_read);
+        if (bytes_wrote != (int32_t)to_read) {
+            logwarn("write error (%" PRId32 " != %" PRId32 ")", bytes_wrote, to_read);
+            break;
+        }
+
+        tracker.update(bytes_read);
+
+        bytes_copied += bytes_read;
     }
 
-    loginfo("done (%u bytes)", total_read);
+    auto elapsed = fk_uptime() - started;
+    auto speed = ((bytes_copied / 1024.0f) / (elapsed / 1000.0f));
+    loginfo("done (%d) (%" PRIu32 "ms) %.2fkbps", bytes_copied, elapsed, speed);
 
     http->close();
 }
