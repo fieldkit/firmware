a7b869798bb56bd428e7659150fc0830c1c3a73d
Jacob Lewallen <jlewallen@gmail.com>
2020-02-29 11:31:55 -0800

    Change default upload interval to an hour.

--- fk/config.h
+++ fk/config.h
@@ -382,7 +382,7 @@ typedef struct configuration_t {
 
     typedef struct scheduler_t {
         uint32_t readings_interval{ 60 };
-        uint32_t network_interval{ 300 * 4 };
+        uint32_t network_interval{ 60 * 60 * 60 };
         uint32_t lora_interval{ 60 * 60 * 2 };
         uint32_t gps_interval{ OneDaySeconds };
 
