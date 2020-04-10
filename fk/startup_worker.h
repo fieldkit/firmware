890b68df0141a16634ff79200b14083130300ee5
Jacob Lewallen <jlewallen@gmail.com>
2020-04-10 12:27:54 -0700

    Hide startup work showing on startup.

--- fk/startup_worker.h
+++ fk/startup_worker.h
@@ -11,6 +11,10 @@ class StartupWorker : public Worker {
 public:
     void run(Pool &pool) override;
 
+    TaskDisplayInfo display_info() const override {
+        return { .name = "", .progress = 0.0f, .visible = false };
+    }
+
 public:
     uint8_t priority() const override {
         return OS_PRIORITY_NORMAL;
