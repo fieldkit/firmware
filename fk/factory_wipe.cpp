6e0e149ab554702c5cadbe73a7ecbe2e52ba901d
Jacob Lewallen <jlewallen@gmail.com>
2020-05-11 11:51:30 -0700

    Nuke old code, unnecessary.

--- fk/factory_wipe.cpp
+++ fk/factory_wipe.cpp
@@ -19,35 +19,6 @@ bool FactoryWipe::wipe(ProgressCallbacks *progress) {
         return false;
     }
 
-    if (false) {
-        auto g = storage_->geometry();
-        auto data_memory = MemoryFactory::get_data_memory();
-
-        if (!data_memory->begin()) {
-            return false;
-        }
-
-        auto consecutive_failures = 10;
-
-        for (auto address = g.beginning(); address < g.end(); address += g.block_size) {
-            if (!data_memory->erase_block(address)) {
-                // These are usually bad blocks.
-                logwarn("error erasing #%" PRIu32, address / g.block_size);
-
-                consecutive_failures--;
-                if (consecutive_failures == 0) {
-                    return false;
-                }
-            }
-            else {
-                logverbose("erased #%" PRIu32, address / g.block_size);
-                consecutive_failures = 0;
-            }
-
-            progress->progress(Operation::Wipe, (address / (float)g.end()) * 100.0f);
-        }
-    }
-
     loginfo("done");
 
     return true;
