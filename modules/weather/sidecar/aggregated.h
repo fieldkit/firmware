a11b80b3ca65dcefa681fff520d90910a2901423
Jacob <jlewalle@gmail.com>
2020-11-18 19:36:10 -0800

    Remove warning.

--- modules/weather/sidecar/aggregated.h
+++ modules/weather/sidecar/aggregated.h
@@ -2,6 +2,8 @@
 
 #include "sidecar.h"
 
+int32_t aggregated_weather_initialize(fk_weather_aggregated_t *aw);
+
 uint32_t aggregated_weather_sign(fk_weather_aggregated_t const *aw);
 
 int32_t aggregated_weather_include(fk_weather_aggregated_t *aw, struct calendar_date_time *clock, fk_weather_t *weather);
