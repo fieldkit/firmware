ea460103a9dc53f4070825c1ba0ae483afebf304
Jacob Lewallen <jlewallen@gmail.com>
2021-02-27 15:51:47 -0800

    Drop logging syscall, va_list stuff won't work.

--- fk/syscalls_app.cpp
+++ fk/syscalls_app.cpp
@@ -38,42 +38,4 @@ void fkos_memory_free(void *ptr) {
     }
 }
 
-#if defined(__SAMD51__)
-
-size_t svc_logv(LogLevels level, const char *facility, const char *f, sys_vargs args) {
-    valogf(level, facility, f, args);
-    return 0;
-}
-
-SVC_4_1(svc_logv, size_t, LogLevels, const char*, const char*, sys_vargs, RET_uint32_t);
-
-size_t fkos_logv(LogLevels level, const char *facility, const char *f, sys_vargs args) {
-    if (osi_in_task()) {
-        return __svc_logv(level, facility, f, args);
-    }
-    else {
-        return svc_logv(level, facility, f, args);
-    }
-}
-
-size_t fkos_logf(LogLevels level, const char *facility, const char *f, ...) {
-    va_list args;
-    va_start(args, f);
-    auto l = fkos_logv(level, facility, f, args);
-    va_end(args);
-    return l;
-}
-
-#else
-
-size_t fkos_logf(LogLevels level, const char *facility, const char *f, ...) {
-    va_list args;
-    va_start(args, f);
-    auto l = vprintf(f, args);
-    va_end(args);
-    return l;
-}
-
-#endif
-
 }
--- fk/syscalls_app.h
+++ fk/syscalls_app.h
@@ -9,10 +9,4 @@ void *fkos_memory_alloc();
 
 void fkos_memory_free(void *ptr);
 
-#define sys_vargs va_list
-
-size_t fkos_logv(LogLevels level, const char *facility, const char *f, sys_vargs args);
-
-size_t fkos_logf(LogLevels level, const char *facility, const char *f, ...) __attribute__((unused)) __attribute__((format(printf, 3, 4)));
-
 }
