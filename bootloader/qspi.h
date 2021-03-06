9360c64900b20ac16961973d1f9bfe64d46ce36c
Jacob Lewallen <jlewallen@gmail.com>
2021-03-06 10:19:46 -0800

    Copied code to initialize memory mapped QSPI chip.

---
+++ bootloader/qspi.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdint.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+/**
+ *
+ */
+int32_t qspi_initialize();
+
+#if defined(__cplusplus)
+} // extern "C"
+#endif
