f0e8ac29ce5508cef401653831d2d5f12fb3399a
Jacob Lewallen <jlewallen@gmail.com>
2021-03-08 20:31:24 -0800

    refact: Wrap setup/config in a LfsDriver class.

--- fk/storage/lfs_driver.cpp
+++ fk/storage/lfs_driver.cpp
@@ -1,14 +1,6 @@
 #include "lfs_driver.h"
 
-#include "hal/memory.h"
-#include "pool.h"
-
 namespace fk {
-
-#define LFS_DRIVER_READ_SIZE             (1024)
-#define LFS_DRIVER_PROG_SIZE             (1024)
-#define LFS_DRIVER_CACHE_SIZE            (LFS_DRIVER_READ_SIZE * 2)
-#define LFS_DRIVER_LOOKAHEAD_SIZE        (16)
 
 FK_DECLARE_LOGGER("lfs");
 
@@ -17,29 +9,13 @@ typedef DataMemory*(*DataMemoryFn)(void);
 // Read a region in a block. Negative error codes are propogated
 // to the user.
 int lfs_block_device_read(struct lfs_config const *c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size) {
-    logdebug("read: %" PRIu32 " off=%" PRIu32 " size=%" PRIu32, block, off, size);
-
-    auto memory = reinterpret_cast<DataMemoryFn>(c->context)();
-    auto g = memory->geometry();
-    auto address = block * g.block_size + off;
-    if (memory->read(address, (uint8_t *)buffer, size) != (int32_t)size) {
-        return -1;
-    }
-
-    return 0;
+    auto driver = reinterpret_cast<LfsDriver*>(c->context);
+    return driver->read(c, block, off, buffer, size);
 }
 
 int lfs_block_device_prog(struct lfs_config const *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size) {
-    logdebug("prog: %" PRIu32 " off=%" PRIu32 " size=%" PRIu32, block, off, size);
-
-    auto memory = reinterpret_cast<DataMemoryFn>(c->context)();
-    auto g = memory->geometry();
-    auto address = block * g.block_size + off;
-    if (memory->write(address, (uint8_t *)buffer, size) != (int32_t)size) {
-        return -1;
-    }
-
-    return 0;
+    auto driver = reinterpret_cast<LfsDriver*>(c->context);
+    return driver->prog(c, block, off, buffer, size);
 }
 
 // Erase a block. A block must be erased before being programmed.
@@ -47,16 +23,8 @@ int lfs_block_device_prog(struct lfs_config const *c, lfs_block_t block, lfs_off
 // are propogated to the user.
 // May return LFS_ERR_CORRUPT if the block should be considered bad.
 int lfs_block_device_erase(struct lfs_config const *c, lfs_block_t block) {
-    logdebug("erase: %" PRIu32, block);
-
-    auto memory = reinterpret_cast<DataMemoryFn>(c->context)();
-    auto g = memory->geometry();
-    auto address = block * g.block_size;
-    if (!memory->erase(address, g.block_size)) {
-        return -1;
-    }
-
-    return 0;
+    auto driver = reinterpret_cast<LfsDriver*>(c->context);
+    return driver->erase(c, block);
 }
 
 // Sync the state of the underlying block device. Negative error codes
@@ -65,53 +33,94 @@ int lfs_block_device_sync(struct lfs_config const *c) {
     return 0;
 }
 
-struct lfs_config cfg = {
-    .context = (void *)MemoryFactory::get_data_memory,
-    .read = lfs_block_device_read,
-    .prog = lfs_block_device_prog,
-    .erase = lfs_block_device_erase,
-    .sync = lfs_block_device_sync,
-    .read_size = LFS_DRIVER_READ_SIZE,
-    .prog_size = LFS_DRIVER_PROG_SIZE,
-    .block_size = 128 * 1024,
-    .block_count = 128, // TODO
-    .block_cycles = 500,
-    .cache_size = LFS_DRIVER_CACHE_SIZE,
-    .lookahead_size = LFS_DRIVER_LOOKAHEAD_SIZE,
-    .read_buffer = nullptr,
-    .prog_buffer = nullptr,
-    .lookahead_buffer = nullptr,
-    .name_max = 0,
-    .file_max = 0,
-    .attr_max = 0,
-    .metadata_max = 0,
-};
+
+LfsDriver::LfsDriver(DataMemory *memory, Pool &pool) : memory_(memory), pool_(&pool) {
+    auto g = memory->geometry();
+
+    cfg_ = {
+        .context = this,
+        .read = lfs_block_device_read,
+        .prog = lfs_block_device_prog,
+        .erase = lfs_block_device_erase,
+        .sync = lfs_block_device_sync,
+        .read_size = LFS_DRIVER_READ_SIZE,
+        .prog_size = LFS_DRIVER_PROG_SIZE,
+        .block_size = g.block_size,
+        .block_count = g.nblocks,
+        .block_cycles = 500,
+        .cache_size = LFS_DRIVER_CACHE_SIZE,
+        .lookahead_size = LFS_DRIVER_LOOKAHEAD_SIZE,
+        .read_buffer = pool_->malloc(LFS_DRIVER_READ_SIZE),
+        .prog_buffer = pool_->malloc(LFS_DRIVER_PROG_SIZE),
+        .lookahead_buffer = pool_->malloc(LFS_DRIVER_LOOKAHEAD_SIZE),
+        .name_max = 0,
+        .file_max = 0,
+        .attr_max = 0,
+        .metadata_max = 0,
+    };
+}
+
+LfsDriver::~LfsDriver() {
+}
+
+bool LfsDriver::begin() {
+    int32_t err = lfs_mount(&lfs_, &cfg_);
+    if (err) {
+        FK_ASSERT(!lfs_format(&lfs_, &cfg_));
+        FK_ASSERT(!lfs_mount(&lfs_, &cfg_));
+    }
+
+    return true;
+}
+
+int32_t LfsDriver::read(struct lfs_config const *c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size) {
+    // logdebug("read: %" PRIu32 " off=%" PRIu32 " size=%" PRIu32, block, off, size);
+
+    auto g = memory_->geometry();
+    auto address = block * g.block_size + off;
+    if (memory_->read(address, (uint8_t *)buffer, size) != (int32_t)size) {
+        return -1;
+    }
+
+    return 0;
+}
+
+int32_t LfsDriver::prog(struct lfs_config const *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size) {
+    // logdebug("prog: %" PRIu32 " off=%" PRIu32 " size=%" PRIu32, block, off, size);
+
+    auto g = memory_->geometry();
+    auto address = block * g.block_size + off;
+    if (memory_->write(address, (uint8_t *)buffer, size) != (int32_t)size) {
+        return -1;
+    }
+
+    return 0;
+}
+
+int32_t LfsDriver::erase(struct lfs_config const *c, lfs_block_t block) {
+    // logdebug("erase: %" PRIu32, block);
+
+    auto g = memory_->geometry();
+    auto address = block * g.block_size;
+    if (!memory_->erase(address, g.block_size)) {
+        return -1;
+    }
+
+    return 0;
+}
 
 int32_t lfs_test() {
     StandardPool pool{ "lfs" };
 
-    lfs_t lfs;
-
     FK_ASSERT(MemoryFactory::get_data_memory()->begin());
 
-    cfg.read_buffer = pool.malloc(cfg.read_size);
-    cfg.prog_buffer = pool.malloc(cfg.prog_size);
-    cfg.lookahead_buffer = pool.malloc(cfg.lookahead_size);
+    LfsDriver lfs_driver{ MemoryFactory::get_data_memory(), pool };
 
-    int32_t err = lfs_mount(&lfs, &cfg);
+    FK_ASSERT(lfs_driver.begin());
 
-    // reformat if we can't mount the filesystem
-    // this should only happen on the first boot
-    if (err) {
-        lfs_format(&lfs, &cfg);
-        lfs_mount(&lfs, &cfg);
-    }
+    auto &lfs = lfs_driver.lfs();
 
-    lfs_file_config file_cfg = {
-        .buffer = pool.malloc(cfg.cache_size),
-        .attrs = nullptr,
-        .attr_count = 0,
-    };
+    lfs_file_config file_cfg = lfs_driver.make_file_cfg();
 
     lfs_file_t file;
     uint32_t boot_count = 0;
@@ -134,7 +143,7 @@ int32_t lfs_test() {
 
     auto fs_blocks = lfs_fs_size(&lfs);
 
-    loginfo("test done: %" PRIu32 " size=%" PRIu32, boot_count, fs_blocks * cfg.block_size);
+    loginfo("test done: %" PRIu32 " fs-blocks=%" PRIu32, boot_count, fs_blocks);
 
     while (1) {
     }
--- fk/storage/lfs_driver.h
+++ fk/storage/lfs_driver.h
@@ -2,8 +2,49 @@
 
 #include <lfs.h>
 
+#include "hal/memory.h"
+#include "pool.h"
+
 namespace fk {
 
 int32_t lfs_test();
+
+#define LFS_DRIVER_READ_SIZE             (1024)
+#define LFS_DRIVER_PROG_SIZE             (1024)
+#define LFS_DRIVER_CACHE_SIZE            (LFS_DRIVER_READ_SIZE * 2)
+#define LFS_DRIVER_LOOKAHEAD_SIZE        (16)
+
+class LfsDriver {
+private:
+    DataMemory *memory_{ nullptr };
+    Pool *pool_{ nullptr };
+    struct lfs_config cfg_;
+    lfs_t lfs_;
+
+public:
+    LfsDriver(DataMemory *memory, Pool &pool);
+    virtual ~LfsDriver();
+
+public:
+    int32_t read(struct lfs_config const *c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size);
+    int32_t prog(struct lfs_config const *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size);
+    int32_t erase(struct lfs_config const *c, lfs_block_t block);
+
+public:
+    bool begin();
+
+    lfs_t &lfs() {
+        return lfs_;
+    }
+
+    lfs_file_config make_file_cfg() {
+        return {
+            .buffer = pool_->malloc(LFS_DRIVER_CACHE_SIZE),
+            .attrs = nullptr,
+            .attr_count = 0,
+        };
+    };
+
+};
 
 }
