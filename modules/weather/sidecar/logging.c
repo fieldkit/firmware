8fd0f706c3d3f8db12e6c35e71984c04cf118b14
Jacob Lewallen <jlewallen@gmail.com>
2021-04-02 08:20:04 -0700

    chore: Allow disabling of logging in samd09 firmware.

--- modules/weather/sidecar/logging.c
+++ modules/weather/sidecar/logging.c
@@ -1,9 +1,12 @@
 #include "logging.h"
 
+#if defined(FK_LOGGING)
+
 int32_t loginfo(const char *f) {
     SEGGER_RTT_WriteString(0, RTT_CTRL_TEXT_GREEN "weather " RTT_CTRL_TEXT_YELLOW "info " RTT_CTRL_RESET);
     SEGGER_RTT_WriteString(0, f);
     SEGGER_RTT_WriteString(0, RTT_CTRL_RESET "\n");
+    return 0;
 }
 
 int32_t loginfof(const char *f, ...) {
@@ -13,12 +16,14 @@ int32_t loginfof(const char *f, ...) {
     SEGGER_RTT_vprintf(0, f, &args);
     SEGGER_RTT_WriteString(0, RTT_CTRL_RESET "\n");
     va_end(args);
+    return 0;
 }
 
 int32_t logerror(const char *f) {
     SEGGER_RTT_WriteString(0, RTT_CTRL_TEXT_GREEN "weather " RTT_CTRL_TEXT_RED "error ");
     SEGGER_RTT_WriteString(0, f);
     SEGGER_RTT_WriteString(0, RTT_CTRL_RESET "\n");
+    return 0;
 }
 
 int32_t logerrorf(const char *f, ...) {
@@ -28,4 +33,25 @@ int32_t logerrorf(const char *f, ...) {
     SEGGER_RTT_vprintf(0, f, &args);
     SEGGER_RTT_WriteString(0, RTT_CTRL_RESET "\n");
     va_end(args);
+    return 0;
 }
+
+#else
+
+int32_t loginfo(const char *f) {
+    return 0;
+}
+
+int32_t loginfof(const char *f, ...) {
+    return 0;
+}
+
+int32_t logerror(const char *f) {
+    return 0;
+}
+
+int32_t logerrorf(const char *f, ...) {
+    return 0;
+}
+
+#endif
