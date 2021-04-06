633a05e5967a7d8d9391453fcfd4f692b866b0f5
Jacob Lewallen <jlewallen@gmail.com>
2021-04-06 15:12:18 -0700

    feat: Always generate random name on factory reset. FK-3615

--- fk/device_name.cpp
+++ fk/device_name.cpp
@@ -11,36 +11,13 @@ namespace fk {
 #include "data/animals.h"
 
 const char *fk_device_name_generate(Pool &pool) {
-    fk_serial_number_t sn;
-    fk_serial_number_get(&sn);
-
-    auto key = phylum::crc32_checksum((uint8_t *)&sn, sizeof(fk_serial_number_t));
-    auto animal_index = key % animals_size;
-    auto adjective_index = key % adjectives_size;
+    auto animal_index = fk_random_i32(0, animals_size);
+    auto adjective_index = fk_random_i32(0, adjectives_size);
     auto animal = animals[animal_index];
     auto adjective = adjectives[adjective_index];
-    auto suffix = key % 100;
+    auto suffix = fk_random_i32(0, 100);
 
     return pool.sprintf("%s %s %d", adjective, animal, suffix);
-}
-
-const char *fk_device_name_printf(char *str, size_t size) {
-    fk_serial_number_t sn;
-    fk_serial_number_get(&sn);
-
-    auto key = phylum::crc32_checksum((uint8_t *)&sn, sizeof(fk_serial_number_t));
-    auto animal_index = key % animals_size;
-    auto adjective_index = key % adjectives_size;
-    auto animal = animals[animal_index];
-    auto adjective = adjectives[adjective_index];
-    auto suffix = key % 100;
-
-    auto needed = tiny_snprintf(str, size, "%s %s %d", adjective, animal, suffix);
-    if ((size_t)needed >= size) {
-        return nullptr;
-    }
-
-    return str;
 }
 
 }
