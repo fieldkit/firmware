2f5537a4a42bdf773eab2117847393467d20200a
Jacob Lewallen <jlewallen@gmail.com>
2021-05-25 08:52:00 -0700

    refact: Remove old, unused debugging code.

--- fk/debugging.h
+++ fk/debugging.h
@@ -16,16 +16,6 @@ bool fk_debug_override_schedules();
 
 void fk_debug_set_console_attached();
 
-#define FK_DEBUG_PREFIX "~"
-
-#define FK_DEBUG_LOG(f, ...)                         \
-    if (fk_debug_get_console_attached()) {           \
-        SEGGER_RTT_LOCK();                           \
-        SEGGER_RTT_WriteString(0, FK_DEBUG_PREFIX);  \
-        SEGGER_RTT_printf(0, f, ##__VA_ARGS__);      \
-        SEGGER_RTT_UNLOCK();                         \
-    }
-
 class DebuggerOfLastResort {
 private:
     static DebuggerOfLastResort instance_;
