b12b907754bb8966bd3bd1be6bb8eaf98baf4f50
Jacob Lewallen <jlewallen@gmail.com>
2021-06-01 17:17:48 -0700

    chore: Rename static to avoid collision.

--- fk/hal/lora.cpp
+++ fk/hal/lora.cpp
@@ -7,13 +7,13 @@
 namespace fk {
 
 #if defined(FK_HARDWARE_FULL)
-static Rn2903LoraNetwork network;
+static Rn2903LoraNetwork lora_network;
 #else
-static NoopLoraNetwork network;
+static NoopLoraNetwork lora_network;
 #endif
 
 LoraNetwork *get_lora_network() {
-    return &network;
+    return &lora_network;
 }
 
 }
