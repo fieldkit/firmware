60223250e84dd01d85ee68547d04ccb6d2e25938
Jacob Lewallen <jlewallen@gmail.com>
2021-10-21 12:14:42 -0700

    refact: Styling, adjust readings menu to 3 decimals.

--- fk/display/readings_view.cpp
+++ fk/display/readings_view.cpp
@@ -10,8 +10,7 @@ namespace fk {
 
 FK_DECLARE_LOGGER("readings");
 
-static void add_reading(collection<DisplayReading> &readings, state::AttachedModule *am, state::AttachedSensor *sensor,
-                        Pool &pool) {
+static void add_reading(collection<DisplayReading> &readings, state::AttachedModule *am, state::AttachedSensor *sensor, Pool &pool) {
     auto module_wo_modules = am->name();
     if (strstr(module_wo_modules, "modules.") == module_wo_modules) {
         module_wo_modules += strlen("modules.");
@@ -80,8 +79,7 @@ template <typename TSelect> struct SensorReadingOption : public MenuOption {
     TSelect select_fn_;
     char buffer_[32];
 
-    SensorReadingOption(uint32_t sensor_index, TSelect select_fn)
-        : MenuOption("..."), sensor_index_(sensor_index), select_fn_(select_fn) {
+    SensorReadingOption(uint32_t sensor_index, TSelect select_fn) : MenuOption("..."), sensor_index_(sensor_index), select_fn_(select_fn) {
     }
 
     void on_selected() override {
@@ -94,15 +92,14 @@ template <typename TSelect> struct SensorReadingOption : public MenuOption {
             auto mas = attached->get_nth_sensor(sensor_index_);
             auto reading = mas.sensor->reading();
             auto position = mas.attached_module->position().integer();
-            logverbose("[%d] refresh: %s %f", sensor_index_, mas.sensor->name(), reading.calibrated);
-            tiny_snprintf(buffer_, sizeof(buffer_), "[%d] %f", position, reading.calibrated);
+            logverbose("[%d] refresh: %s %.3f", sensor_index_, mas.sensor->name(), reading.calibrated);
+            tiny_snprintf(buffer_, sizeof(buffer_), "[%d] %.3f", position, reading.calibrated);
             label_ = buffer_;
         }
     }
 };
 
-template <typename TSelect>
-SensorReadingOption<TSelect> *to_sensor_reading_option(Pool *pool, uint32_t sensor_index, TSelect fn) {
+template <typename TSelect> SensorReadingOption<TSelect> *to_sensor_reading_option(Pool *pool, uint32_t sensor_index, TSelect fn) {
     return new (*pool) SensorReadingOption<TSelect>(sensor_index, fn);
 }
 
@@ -112,13 +109,13 @@ MenuScreen *create_readings_menu(GlobalState const *gs, MenuOption *back_option,
         return nullptr;
     }
 
-    auto nsensors = attached->number_of_physical_sensors();
+    auto nsensors = attached->number_of_sensors();
     auto options = (MenuOption **)pool.malloc(sizeof(MenuOption *) * (nsensors + 2));
     auto sensor_index = 0u;
     auto option_index = 0u;
 
     for (auto &attached_module : attached->modules()) {
-        if (!attached_module.is_virtual()) {
+        if (true || !attached_module.is_virtual()) {
             for (auto &sensor : attached_module.sensors()) {
                 FK_ASSERT(option_index < nsensors);
                 auto option = to_sensor_reading_option(&pool, sensor_index, [=]() { (void)sensor; });
