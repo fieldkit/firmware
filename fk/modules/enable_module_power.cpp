d267f440180e44f4cf5434514b72acb473ef5e3b
Jacob Lewallen <jlewallen@gmail.com>
2022-01-21 14:13:13 -0800

    feat: Log module on time.

--- fk/modules/enable_module_power.cpp
+++ fk/modules/enable_module_power.cpp
@@ -14,6 +14,13 @@ EnableModulePower::~EnableModulePower() {
         if (!get_modmux()->disable_module(position_)) {
             logerror("[%d] disabling module failed", position_.integer());
         }
+
+        auto off_time = fk_uptime();
+        if (enabled_at_ > 0) {
+            loginfo("[%d] module on for %" PRIu32, position_.integer(), off_time - enabled_at_);
+        } else {
+            logerror("[%d] module unexpectedly on", position_.integer());
+        }
     } else {
         logdebug("[%d] leaving alone", position_.integer());
     }
@@ -32,18 +39,18 @@ bool EnableModulePower::always_enabled() {
 }
 
 bool EnableModulePower::was_enabled() {
-    return was_enabled_;
+    return enabled_at_ > 0;
 }
 
 bool EnableModulePower::enable() {
     auto mm = get_modmux();
 
-    was_enabled_ = false;
+    enabled_at_ = 0;
 
     if (enabled_once() || always_enabled()) {
         if (!mm->is_module_on(position_)) {
             logdebug("[%d] powering on", position_.integer());
-            was_enabled_ = true;
+            enabled_at_ = fk_uptime();
         }
 
         // Updates power flag even if the module is already on.
@@ -53,7 +60,7 @@ bool EnableModulePower::enable() {
             return false;
         }
 
-        if (was_enabled_ && wake_delay_ > 0) {
+        if (enabled_at_ > 0 && wake_delay_ > 0) {
             logdebug("[%d] wake delay: %" PRIu32 "ms", position_.integer(), wake_delay_);
             fk_delay(wake_delay_);
         }
