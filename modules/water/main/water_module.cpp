34e186596c0f31aeff902cfeb38b5dc7485aa8d7
Jacob Lewallen <jlewallen@gmail.com>
2022-03-25 11:50:31 -0700

    fix: New default exponential EC curve.

--- modules/water/main/water_module.cpp
+++ modules/water/main/water_module.cpp
@@ -350,8 +350,8 @@ Curve *WaterModule::create_modules_default_curve(Pool &pool) {
         return create_curve(fk_data_CurveType_CURVE_LINEAR, OrpDefaultCalibration, pool);
     }
     case FK_MODULES_KIND_WATER_EC: {
-        constexpr float EcDefaultCalibration[3] = { 3432.6 - 3.636, 0 };
-        return create_curve(fk_data_CurveType_CURVE_POWER, EcDefaultCalibration, pool);
+        constexpr float EcDefaultCalibration[3] = { 1013.407233, 235718422.3, -10.66457333 };
+        return create_curve(fk_data_CurveType_CURVE_EXPONENTIAL, EcDefaultCalibration, pool);
     }
     default:
         return create_noop_curve(pool);
