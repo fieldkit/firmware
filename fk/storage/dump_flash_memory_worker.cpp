29ef6a16100bcbdec2833ae6c577db8556cdaec9
Jacob Lewallen <jlewallen@gmail.com>
2022-05-09 14:38:14 -0700

    feat: Dhara compatible flash memory dump.

--- fk/storage/dump_flash_memory_worker.cpp
+++ fk/storage/dump_flash_memory_worker.cpp
@@ -57,10 +57,45 @@ void DumpFlashMemoryWorker::run(Pool &pool) {
     gsm.notify("flash copied");
 }
 
-void DumpFlashMemoryWorker ::dump_phylum_storage(SdCardFile *file, Pool &pool) {
+void DumpFlashMemoryWorker::dump_phylum_storage(SdCardFile *file, Pool &pool) {
+    auto flash_memory = MemoryFactory::get_data_memory();
+    if (!flash_memory->begin()) {
+        logerror("error opening flash memory");
+        return;
+    }
+
+    auto sector_size = flash_memory->geometry().real_page_size;
+    standard_page_buffer_memory buffer_memory{ &pool };
+    phylum::working_buffers buffers{ &buffer_memory, sector_size, 8 };
+    phylum::noop_page_cache page_cache;
+    PhylumFlashMemory memory(flash_memory, &buffers);
+    phylum::dhara_sector_map sectors{ buffers, memory, &page_cache };
+
+    if (sectors.begin(false) < 0) {
+        logerror("error initializing sector map");
+        return;
+    }
+
+    auto buffer = (uint8_t *)pool.malloc(sector_size);
+    auto nsectors = sectors.size();
+
+    for (auto sector = 0u; nsectors; ++sector) {
+        auto err = sectors.read(sector, buffer, sector_size);
+        if (err < 0) {
+            logerror("error reading sector %d (%d)", sector, err);
+            return;
+        }
+
+        if (file->write(buffer, sector_size) == 0) {
+            logerror("error writing to file");
+            return;
+        }
+    }
+
+    loginfo("done writing %d sectors (%d bytes)", nsectors, nsectors * sector_size);
 }
 
-void DumpFlashMemoryWorker ::dump_darwin_storage(SdCardFile *file, Pool &pool) {
+void DumpFlashMemoryWorker::dump_darwin_storage(SdCardFile *file, Pool &pool) {
     auto flash_memory = MemoryFactory::get_data_memory();
     if (!flash_memory->begin()) {
         logerror("error opening flash memory");
